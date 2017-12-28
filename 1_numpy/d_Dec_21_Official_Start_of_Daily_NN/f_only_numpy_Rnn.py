b53cb7c378a64f2065a6a065aa3ce25ce8c5c29b
JDSeo <jae.duk.seo@gmail.com>
2017-12-28 02:24:52 -0500

    RNN failed

--- 1_numpy/d_Dec_21_Official_Start_of_Daily_NN/f_only_numpy_Rnn.py
+++ 1_numpy/d_Dec_21_Official_Start_of_Daily_NN/f_only_numpy_Rnn.py
@@ -2,11 +2,20 @@ import numpy as np
 
 np.random.seed(1234)
 
+
+#  this is failure attempt
+
 def log(x):
     return 1 / ( 1+ np.exp( -1 * x ))
 
 def d_log(x):
     return log(x) * (1 - log(x))
+
+def tanh(x):
+    return np.tanh(x)
+
+def d_tanh(x):
+    return 1 - tanh(x) ** 2
 
 x = np.array([
     [0,0,0],
@@ -27,9 +36,9 @@ wx = 1.0
 wrec = 1.0
 
 
-number_of_epoch = 50000
+number_of_epoch = 90000
 
-lr_wx = 0.001
+lr_wx = 0.00001
 lr_wrec = 0.00003
 
 state = np.zeros((x.shape[0],x.shape[1] + 1))
@@ -40,24 +49,24 @@ vx,vrec = 0,0
 for iter in range(number_of_epoch):
 
     state_1_in  = state[:,0]*wrec + x[:,0]*wx
-    state_1_out = log(state_1_in)
+    state_1_out = tanh(state_1_in)
     state[:,1] = state_1_out
 
     state_2_in  = state[:,1]*wrec + x[:,1]*wx
-    state_2_out = log(state_2_in)
+    state_2_out = tanh(state_2_in)
     state[:,2] = state_2_out
 
     state_3_in  = state[:,2]*wrec + x[:,2]*wx
     state[:,3] = state_3_in
 
-    cost = np.square(state[:,3] - np.squeeze(y)).sum() / len(x)
+    cost = np.square(state[:,3] - np.squeeze(y)).sum() / 2
 
     if iter % 1000 == 0:
         print("Current iter : ", iter, " Current cost: ", cost)
 
-    grad_over_time[:,2] = (state[:,3] - np.squeeze(y)) * (2/len(x))
-    grad_over_time[:,1] = grad_over_time[:,2] * wrec  * d_log(state_2_in)
-    grad_over_time[:,0] = grad_over_time[:,1] * wrec  * d_log(state_1_in)
+    grad_over_time[:,2] = state[:,3] - np.squeeze(y)
+    grad_over_time[:,1] = grad_over_time[:,2] * wrec  * d_tanh(state_2_in)
+    grad_over_time[:,0] = grad_over_time[:,1] * wrec  * d_tanh(state_1_in)
 
     grad_wx = np.sum(grad_over_time[:,2]*x[:,2]+
                     grad_over_time[:,1]*x[:,1]+
