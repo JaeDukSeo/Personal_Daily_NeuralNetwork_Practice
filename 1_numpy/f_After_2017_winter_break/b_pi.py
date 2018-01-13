2082cb3dcd2264e10e21b4f474a93f31b2b0c080
JDSeo <jae.duk.seo@gmail.com>
2018-01-13 10:48:35 -0500

    Prepare Pixel RNN

---
+++ 1_numpy/f_After_2017_winter_break/b_pi.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+print(np.prod([[1.,2.,3.,4.]]))
+
+product_array = [1,3,4,5]
+
+# Func: Math Notation PI
+print(np.prod(product_array))
+
+# Func: Math Notation Sum
+print(np.sum(product_array))
+
+
+#  -- end code --
