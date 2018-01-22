698197a359722e1f79f78250ba023d3f8d0aada5
JDSeo <jae.duk.seo@ryerson.ca>
2018-01-21 23:08:42 -0500

    s

--- 9_Code_from_other_ppl/som.py
+++ 9_Code_from_other_ppl/som.py
@@ -40,42 +40,7 @@ if normalise_data:
         # normalise entire dataset
         data = raw_data / data.max()
 
-def find_bmu(t, net, m):
-    """
-        Find the best matching unit for a given vector, t, in the SOM
-        Returns: a (bmu, bmu_idx) tuple where bmu is the high-dimensional BMU
-                 and bmu_idx is the index of this vector in the SOM
-    """
-    bmu_idx = np.array([0, 0])
-    # set the initial minimum distance to a huge number
-    min_dist = np.iinfo(np.int).max
-    # calculate the high-dimensional distance between each neuron and the input
-    for x in range(net.shape[0]):
-        for y in range(net.shape[1]):
-            w = net[x, y, :].reshape(m, 1)
-            # don't bother with actual Euclidean distance, to avoid expensive sqrt operation
-            sq_dist = np.sum((w - t) ** 2)
-            if sq_dist < min_dist:
-                min_dist = sq_dist
-                bmu_idx = np.array([x, y])
-    # get vector corresponding to bmu_idx
-    bmu = net[bmu_idx[0], bmu_idx[1], :].reshape(m, 1)
-    # return the (bmu, bmu_idx) tuple
-    return (bmu, bmu_idx)
 
-
-# ---- radius and learning rate ------
-def decay_radius(initial_radius, i, time_constant):
-    return initial_radius * np.exp(-i / time_constant)
-
-def decay_learning_rate(initial_learning_rate, i, n_iterations):
-    return initial_learning_rate * np.exp(-i / n_iterations)
-# ---- radius and learning rate ------
-
-# --------- what is this?? -----
-def calculate_influence(distance, radius):
-    return np.exp(-distance / (2* (radius**2)))
-# --------- what is this?? -----
 
 
 # ---- Training Aspect -----
@@ -86,26 +51,12 @@ for i in range(n_iterations):
 
     temp = np.subtract(net,np.squeeze(t)) ** 2
     temp_sum = temp.sum(axis=2)
-    bmu_idx_2 = np.squeeze(np.asarray(np.where(temp_sum == temp_sum.min())))
-    bmu_2 = net[bmu_idx_2[0],bmu_idx_2[1],:].reshape(np.array([m, 1]))
-
-    # find its Best Matching Unit
-    # bmu, bmu_idx = find_bmu(t, net, m)
-
-    bmu  = bmu_2
-    bmu_idx  = bmu_idx_2
-
-    # print("My : ",bmu_idx_2,bmu_2)
-    # print("\noG : ",bmu_idx,bmu)
-    # print('\n')
-    # print(bmu_2-bmu)
-    # print('\n')
-    # print(bmu_idx_2-bmu_idx)
-    # tsss = input()
+    bmu_idx = np.squeeze(np.asarray(np.where(temp_sum == temp_sum.min())))
+    bmu = net[bmu_idx[0],bmu_idx[1],:].reshape(np.array([m, 1]))
 
     # decay the SOM parameters
-    r = decay_radius(init_radius, i, time_constant)
-    l = decay_learning_rate(init_learning_rate, i, n_iterations)
+    r = init_radius * np.exp(-i / time_constant)
+    l = init_learning_rate * np.exp(-i / n_iterations)
 
     # now we know the BMU, update its weight vector to move closer to input
     # and move its neighbours in 2-D space closer
@@ -118,7 +69,7 @@ for i in range(n_iterations):
             # if the distance is within the current neighbourhood radius
             if w_dist <= r**2:
                 # calculate the degree of influence (based on the 2-D distance)
-                influence = calculate_influence(w_dist, r)
+                influence = np.exp(-w_dist / (2* (r**2)))
                 # now update the neuron's weight using the formula:
                 # new w = old w + (learning rate * influence * delta)
                 # where delta = input vector (t) - old w
@@ -127,10 +78,6 @@ for i in range(n_iterations):
                 net[x, y, :] = new_w.reshape(1, 3)
 
 
-print('\n-----')
-print(raw_data.shape)
-print(net.shape)
-print('\n-----')
 
 
 fig = plt.figure()
@@ -151,6 +98,37 @@ plt.show()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------- trash -------
 
 
 sys.exit()
