3a664492100f76528569e415e2f877b3bf01386d
JDSeo <jae.duk.seo@ryerson.ca>
2018-01-25 14:44:12 -0500

    s

--- 1_numpy/e_vgg/a_vgg.py
+++ 1_numpy/e_vgg/a_vgg.py
@@ -1,6 +1,11 @@
 import numpy as np
 
+# Func: Only for 2D convolution 
+from scipy.signal import convolve2d
 
+# Func: For Back propagation on Max Pooling
+from scipy.ndimage.filters import maximum_filter
+import skimage.measure
 
 
 
