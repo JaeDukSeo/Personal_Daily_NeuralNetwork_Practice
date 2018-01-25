ee05eb3922162dbe9479863d7c2f179c35b4ab2e
JDSeo <jae.duk.seo@ryerson.ca>
2018-01-25 13:05:58 -0500

    size

--- 1_numpy/g_rnn_special/g.py
+++ 1_numpy/g_rnn_special/g.py
@@ -20,12 +20,48 @@ print(maximum_filter(arr,size=4))
 
 
 
+temp = np.array([
+    [2,3,2,4],
+    [8,3,12,1],
+    [12,3,4,9],
+    [90,2,-8,12]
+])
 
 
+sss = skimage.measure.block_reduce(temp, (2,2), np.max)
+print('----------------')
+print(temp)
+print('----------------')
+print(sss)
+print('----------------')
+print( np.where(temp == temp.max(axis=1) ))
+print('----------------')
+print('----------------')
+
+#forward
+activationPrevious = np.copy(temp)
+activations = skimage.measure.block_reduce(temp, block_size=(2,2), func=np.max)
+maxs = activations.repeat(2, axis=0).repeat(2, axis=1)
+mask = np.equal(activationPrevious, maxs).astype(int)
 
 
+print(temp)
+print(activations)
+print(maxs)
+print(mask)
+print('----------------')
+print('----------------')
 
 
+delta = activations.repeat(2, axis=0).repeat(2, axis=1)
+print(delta)
+
+delta = np.multiply(delta, mask)
+print(delta)
+
+#backward
+# delta = delta.repeat(2, axis=2).repeat(2, axis=3)
+# delta = np.multiply(delta, mask)
 
 
 # --- enc code ---
