e0151c88446e6d2d4c1c2a5003228126ed79674a
JDSeo <jae.duk.seo@ryerson.ca>
2018-01-30 16:19:21 -0500

    Attemp

--- 1_numpy/j_GAN/a.py
+++ 1_numpy/j_GAN/a.py
@@ -43,18 +43,15 @@ images_test,labels_test  = images[:1000,:],onehot_label[:1000]
 images_train,labels_train  = images[1000:,:],onehot_label[1000:]
 
 # 2. Hyper parameters
-num_epoch = 500
-learning_rate = 0.000001 
+num_epoch = 800
+learning_rate = 0.001 
 
-D_w1 = np.random.randn(784,128) * 0.0002
-D_w2 = np.random.randn(128,1) *  0.0002
+D_w1 = np.random.randn(784,560) * 0.002
+D_w2 = np.random.randn(560,1) *  0.002
 
-G_w1 = np.random.randn(100,128) *  0.0002
-G_w2 = np.random.randn(128,784) *  0.0002
+G_w1 = np.random.randn(100,560) *  0.002
+G_w2 = np.random.randn(560,784) *  0.002
 
-
-def sample_Z(m, n):
-    return np.random.uniform(-1., 1., size=[m, n])
 
 def plot(samples):
     fig = plt.figure(figsize=(4, 4))
@@ -73,10 +70,12 @@ def plot(samples):
 
 for iter in range(num_epoch):
 
-    for batch_size in range(0,len(images_train),10):
+    for batch_size in range(0,len(images_train),100):
         
-        current_image = images_train[batch_size:batch_size+10,:]
-        curren_generated = sample_Z(10,100)
+        # current_image = np.expand_dims(images_train[batch_size,:],axis=0)
+        current_image = images_train[batch_size:batch_size+100,:]
+        
+        curren_generated = np.random.uniform(-1., 1., size=[1, 100])
         
         G_l1 = curren_generated.dot(G_w1)
         G_h1 = ReLu(G_l1)
@@ -93,24 +92,20 @@ for iter in range(num_epoch):
         D_logit_f = D_h1_f.dot(D_w2)
         D_fake = log(D_logit_f)
 
-        D_loss = -1 * np.log(D_real) - np.log( 1 - D_fake)
-        G_loss = -1 * np.log(D_fake)
-
+        D_loss = (-1 * np.log(D_real) - np.log( 1 - D_fake))/100
+        G_loss = (-1 * np.log(D_fake))/100
         print("Current iter: ",iter, "  Current batch: ",batch_size, " D loss: ",D_loss.sum(), " G Loss: ", G_loss.sum(),end='\r')
 
-
-        grad_D_part_r_1 = -1 * (1/D_real) 
+        grad_D_part_r_1 = -1 * (1/D_real) /100
         grad_D_part_r_2 = d_log(D_logit_r)
         grad_D_part_r_3 = D_h1_r
         grad_D_r = grad_D_part_r_3.T.dot(grad_D_part_r_1 * grad_D_part_r_2)
 
-        grad_D_part_f_1 = -1 * (1/( 1 - D_fake)) 
+        grad_D_part_f_1 = -1 * (1/( 1 - D_fake)) /100
         grad_D_part_f_2 = d_log(D_logit_f)
         grad_D_part_f_3 = D_h1_f
         grad_D_f = grad_D_part_f_3.T.dot(grad_D_part_f_1 * grad_D_part_f_2)
         grad_D_w2 = grad_D_r + grad_D_f
-
-
 
         grad_D_part_r_w1_1 = (grad_D_part_r_1 * grad_D_part_r_2).dot(D_w2.T)
         grad_D_part_r_w1_2 = d_ReLu(D_l1_r)
@@ -123,8 +118,7 @@ for iter in range(num_epoch):
         grad_D_f_w1 = grad_D_part_f_w1_3.T.dot(grad_D_part_f_w1_1 * grad_D_part_f_w1_2)
         grad_D_w1 = grad_D_r_w1 + grad_D_f_w1
 
-
-        grad_G_part_1 = ((-1 * (1/D_fake) * (1/D_logit_f)).dot(D_w2.T) * d_ReLu(D_l1_f)).dot(D_w1.T)
+        grad_G_part_1 = ((-1 * (1/D_fake) * (1/D_logit_f)/100).dot(D_w2.T) * d_ReLu(D_l1_f)).dot(D_w1.T)
         grad_G_part_2 = d_log(G_log_prob)
         grad_G_part_3 = G_h1
         grad_G_w2 = grad_G_part_3.T.dot(grad_G_part_1 * grad_G_part_2)
@@ -134,16 +128,15 @@ for iter in range(num_epoch):
         grad_G_part_3_w1 = curren_generated
         grad_G_w1 = grad_G_part_3_w1.T.dot(grad_G_part_1_w1 * grad_G_part_2_w1)
 
-
         D_w2 = D_w2 - learning_rate * grad_D_w2
         D_w1 = D_w1 - learning_rate * grad_D_w1
 
         G_w1 = G_w1 - learning_rate * grad_G_w1
         G_w2 = G_w2 - learning_rate * grad_G_w2
 
-    if iter % 10 == 0:
+    if iter % 40 == 0:
         
-        curren_generated = sample_Z(16,100)
+        curren_generated = np.random.uniform(-1., 1., size=[16,100])
         
         G_l1 = curren_generated.dot(G_w1)
         G_h1 = ReLu(G_l1)
