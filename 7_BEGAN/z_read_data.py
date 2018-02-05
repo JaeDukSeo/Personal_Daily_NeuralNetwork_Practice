61b543897dc7a64809e42d60e99b83d733162f7d
JDSeo <jae.duk.seo@ryerson.ca>
2018-02-05 11:52:40 -0500

    's'

--- 7_BEGAN/z_read_data.py
+++ 7_BEGAN/z_read_data.py
@@ -1,10 +1,14 @@
 import csv,sys
-
+import requests
+i = 0
 with open('gif_data/short_data.tsv') as tsvfile:
     reader = csv.reader(tsvfile, delimiter='\t')
     for row in reader:
-        print(row[0])
-        print(row[1])
-        
 
-        sys.exit(0)
+        with open('gif_data_download/'+str(i)+'_'+str(row[1])+'.gif', 'wb') as f:
+            f.write(requests.get(row[0]).content)
+        
+        i = i + 1
+        if i == 500:
+            print("stop")
+            sys.exit()
