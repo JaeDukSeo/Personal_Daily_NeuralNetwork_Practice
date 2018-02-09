ce9782c7159c7c165f2e6a9d34535dbd0c131a6b
JDSeo <jae.duk.seo@ryerson.ca>
2018-02-09 18:00:48 -0500

    'Final;y'

--- c_lstm/c_prepare_data.py
+++ c_lstm/c_prepare_data.py
@@ -1,7 +1,6 @@
 from PIL import Image
-import numpy as np,os
+import numpy as np,os,sys,cv2
 from matplotlib import pyplot as plt
-
 
 Pathgif = "./data/dog_preprocessed/"
 dog_gif = []  # create an empty list
@@ -10,7 +9,6 @@ for dirName, subdirList, fileList in os.walk(Pathgif):
         if ".gif" in filename.lower():  # check whether the file's DICOM
             dog_gif.append(os.path.join(dirName,filename))
 
-
 Pathgif = "./data/baby_preprocessed/"
 baby_gif = []  # create an empty list
 for dirName, subdirList, fileList in os.walk(Pathgif):
@@ -18,10 +16,17 @@ for dirName, subdirList, fileList in os.walk(Pathgif):
         if ".gif" in filename.lower():  # check whether the file's DICOM
             baby_gif.append(os.path.join(dirName,filename))
 
-print(baby_gif)
-print(dog_gif)
+dog_array = np.zeros((24,100,100,3)).astype(int)
+store_index = 0
+for element in dog_gif:
+    img = Image.open(element) 
+    for iter in range(img.n_frames):
+        img.seek(iter)
+        new_frame = np.array(img.convert('RGB'))
+        dog_array[store_index,:,:,:] = new_frame
+        store_index = store_index + 1
 
-
-
-
+for iter in range(len(dog_array)):
+    plt.imshow(np.uint8(dog_array[iter,:,:,:]))
+    plt.pause(0.07)
 # -- end code --
