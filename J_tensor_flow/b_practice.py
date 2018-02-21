2d52ba397594bec71cb709cbd7bc390d078b5d76
JDSeo <jae.duk.seo@ryerson.ca>
2018-02-21 04:14:54 -0500

    's'

--- J_tensor_flow/b_practice.py
+++ J_tensor_flow/b_practice.py
@@ -39,6 +39,8 @@ training_images,training_lables =images[test_image_num:test_image_num + training
 learning_rate= 0.001
 num_epoch =300
 batch_size = 100
+print_size = 50
+
 # 2. Build a model with class
 class simple_FCC():
     
@@ -79,13 +81,13 @@ class simple_FCC():
 
         return grad_pass,update_w
 
+
+
+# =============Manual Back Propagation ======================
 # 3. Delcare the Model
 layer1 = simple_FCC(tf_log,d_tf_log,input_dim = 784,ouput_dim = 825)
 layer2 = simple_FCC(tf_arctan,d_tf_arctan,input_dim = 825,ouput_dim = 1024)
 layer3 = simple_FCC(tf_tanh,d_tf_tanh,input_dim = 1024,ouput_dim = 10)
-
-temp = np.ones((1,784))
-temp2 = np.ones((1,10))
 
 # 4. Build a Graph
 x = tf.placeholder(tf.float32, shape=(None, 784))
@@ -97,7 +99,6 @@ l3 = layer3.feed_forward(l2)
 l3Soft = tf_softmax(l3)
 
 cost = tf.reduce_sum( -1 * ( y*tf.log(l3Soft) + (1-y) * tf.log(1-l3Soft ) ) )
-auto_dif = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
 
 gradient3,weightup_3 = layer3.back_propagation(l3Soft - y,3)
 gradient2,weightup_2 = layer2.back_propagation(gradient3,2)
@@ -105,8 +106,8 @@ gradient1,weightup_1 = layer1.back_propagation(gradient2,1)
 weight_update = [weightup_3,weightup_2,weightup_1]
 
 total_cost = 0 
-
-# 4. Run the session
+manual_cost = []
+# 4. Run the session - Manual
 with tf.Session() as sess:
 
     sess.run(tf.global_variables_initializer())
@@ -117,13 +118,12 @@ with tf.Session() as sess:
             current_batch = training_images[current_image_batch:current_image_batch+batch_size,:]
             current_label = training_lables[current_image_batch:current_image_batch+batch_size,:]
 
-            # sess_result = sess.run([cost,gradient1,weight_update],feed_dict={x:current_batch,y:current_label })
-            sess_result = sess.run([cost,auto_dif],feed_dict={x:current_batch,y:current_label })
+            sess_result = sess.run([cost,gradient1,weight_update],feed_dict={x:current_batch,y:current_label })
             total_cost = total_cost +sess_result[0]
             print("Real Time Cost Update Iter: ",iter, "  Cost : ",sess_result[0],end='\r' )
             
 
-        if iter % 10 == 0 :
+        if iter % print_size == 0 :
             print('\n--------------------')
             print("Current Iter: ", iter, " Current total cost: ",total_cost)
             current_batch = testing_images[0:0+2,:]
@@ -138,17 +138,128 @@ with tf.Session() as sess:
             )
             print('--------------------')
 
+        manual_cost.append(total_cost/len(training_images))
         total_cost = 0
+# =============Manual Back Propagation ======================
+
+print('\n===== MOVING ON TO THE NEXT============\n')
+
+# =============Auto Back Propagation ======================
+# 3. Delcare the Model
+layer1_auto = simple_FCC(tf_log,d_tf_log,input_dim = 784,ouput_dim = 825)
+layer2_auto = simple_FCC(tf_arctan,d_tf_arctan,input_dim = 825,ouput_dim = 1024)
+layer3_auto = simple_FCC(tf_tanh,d_tf_tanh,input_dim = 1024,ouput_dim = 10)
+
+# 4. Build a Graph
+x_auto = tf.placeholder(tf.float32, shape=(None, 784))
+y_auto = tf.placeholder(tf.float32, shape=(None,10))
+
+l1_auto = layer1_auto.feed_forward(x_auto)
+l2_auto = layer2_auto.feed_forward(l1_auto)
+l3_auto = layer3_auto.feed_forward(l2_auto)
+l3Soft_auto = tf_softmax(l3)
+
+cost_auto = tf.reduce_sum( -1 * ( y*tf.log(l3Soft) + (1-y) * tf.log(1-l3Soft ) ) )
+auto_dif = tf.train.GradientDescentOptimizer(0.001).minimize(cost_auto)
+
+total_cost = 0 
+auto_cost = []
+# 4. Run the session - Manual
+with tf.Session() as sess:
+
+    sess.run(tf.global_variables_initializer())
+
+    for iter in range(num_epoch) :
+        for current_image_batch in range(0,len(training_images),batch_size):
+            
+            current_batch = training_images[current_image_batch:current_image_batch+batch_size,:]
+            current_label = training_lables[current_image_batch:current_image_batch+batch_size,:]
+
+            sess_result = sess.run([cost_auto,auto_dif],feed_dict={x:current_batch,y:current_label })
+            total_cost = total_cost +sess_result[0]
+            print("Real Time Cost Update Iter: ",iter, "  Cost : ",sess_result[0],end='\r' )
+
+        if iter % print_size == 0 :
+            print('\n--------------------')
+            print("Current Iter: ", iter, " Current total cost: ",total_cost)
+            current_batch = testing_images[0:0+2,:]
+            current_label = testing_lables[0:0+2,:]
+
+            sess_result = sess.run([cost_auto,l3Soft_auto],feed_dict={x:current_batch,y:current_label })
+            print(
+            'Current Predict : ',sess_result[1][0],
+            'GT : ',current_label[0],'\n'
+            'Current Predict : ',sess_result[1][1],
+            'GT : ',current_label[1]
+            )
+            print('--------------------')
+
+        auto_cost.append(total_cost/len(training_images))
+        total_cost = 0
+# =============Auto Back Propagation ======================
+
+print('\n===== MOVING ON TO THE NEXT============\n')
 
 
 
+# =============Dilated Back Propagation ======================
+# 3. Delcare the Model
+layer1_dil = simple_FCC(tf_log,d_tf_log,input_dim = 784,ouput_dim = 825)
+layer2_dil = simple_FCC(tf_arctan,d_tf_arctan,input_dim = 825,ouput_dim = 1024)
+layer3_dil = simple_FCC(tf_tanh,d_tf_tanh,input_dim = 1024,ouput_dim = 10)
 
+# 4. Build a Graph
+x_dil = tf.placeholder(tf.float32, shape=(None, 784))
+y_dil = tf.placeholder(tf.float32, shape=(None,10))
 
+l1_dil = layer1_dil.feed_forward(x)
+l2_dil = layer2_dil.feed_forward(l1)
+l3_dil = layer3_dil.feed_forward(l2)
+l3Soft_dil = tf_softmax(l3)
 
+cost = tf.reduce_sum( -1 * ( y*tf.log(l3Soft) + (1-y) * tf.log(1-l3Soft ) ) )
 
+gradient3,weightup_3 = layer3.back_propagation(l3Soft - y,3)
+gradient2,weightup_2 = layer2.back_propagation(gradient3,2)
+gradient1,weightup_1 = layer1.back_propagation(gradient2,1)
+weight_update = [weightup_3,weightup_2,weightup_1]
 
+total_cost = 0 
+manual_cost = []
+# 4. Run the session - Manual
+with tf.Session() as sess:
 
+    sess.run(tf.global_variables_initializer())
 
+    for iter in range(num_epoch) :
+        for current_image_batch in range(0,len(training_images),batch_size):
+            
+            current_batch = training_images[current_image_batch:current_image_batch+batch_size,:]
+            current_label = training_lables[current_image_batch:current_image_batch+batch_size,:]
+
+            sess_result = sess.run([cost,gradient1,weight_update],feed_dict={x:current_batch,y:current_label })
+            total_cost = total_cost +sess_result[0]
+            print("Real Time Cost Update Iter: ",iter, "  Cost : ",sess_result[0],end='\r' )
+            
+
+        if iter % print_size == 0 :
+            print('\n--------------------')
+            print("Current Iter: ", iter, " Current total cost: ",total_cost)
+            current_batch = testing_images[0:0+2,:]
+            current_label = testing_lables[0:0+2,:]
+
+            sess_result = sess.run([cost,l3Soft],feed_dict={x:current_batch,y:current_label })
+            print(
+            'Current Predict : ',sess_result[1][0],
+            'GT : ',current_label[0],'\n'
+            'Current Predict : ',sess_result[1][1],
+            'GT : ',current_label[1]
+            )
+            print('--------------------')
+
+        manual_cost.append(total_cost/len(training_images))
+        total_cost = 0
+# =============Dilated Back Propagation ======================
 
 
 
