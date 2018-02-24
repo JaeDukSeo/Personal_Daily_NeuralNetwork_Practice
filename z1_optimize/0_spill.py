005ccecbb4b65b05af63359c6cc3a4907ee18e89
JDSeo <jae.duk.seo@ryerson.ca>
2018-02-24 11:38:05 -0500

    'ququ'

--- z1_optimize/0_spill.py
+++ z1_optimize/0_spill.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time 
+from collections import deque
 
 global m,n,surface
 
@@ -57,20 +58,42 @@ def spill2(x,y,stren):
 
 # This is really slow, since the strength is also in the way. 
 # It takes 6 seconds lol
-m = 13  
-n = 13
+m = 7  
+n = 7
 surface = np.zeros((m,n))
 start_time = time.time()
 spill(7,7,8)
 print("--- %s seconds ---" % (time.time() - start_time))
 
 # Short cut actually make it worse LOL
-m = 13  
-n = 13
+m = 7  
+n = 7
 surface = np.zeros((m,n))
 start_time = time.time()
 spill2(7,7,8)
 print("--- %s seconds ---" % (time.time() - start_time))
 
+# Depth first search
+
+
+
+
+
+
+
+
+#Creating a Queue
+queue = deque([1,5,8,9])
+
+#Enqueuing elements to the Queue
+queue.append(7) #[1,5,8,9,7]
+queue.append(0) #[1,5,8,9,7,0]
+
+#Dequeuing elements from the Queue
+queue.popleft() #[5,8,9,7,0]
+queue.popleft() #[8,7,9,0]
+
+#Printing the elements of the Queue
+print(queue)
 
 # -- end code -------
