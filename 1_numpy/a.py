7695f948ff55400d62eef89f0588d42ea08f1113
JDSeo <jae.duk.seo@ryerson.ca>
2018-02-27 21:31:22 -0500

    ''s

---
+++ 1_numpy/a.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt,os,sys
+from sklearn.utils import shuffle
+from scipy.ndimage import imread
+from numpy import float32
+# -2. Set the Random Seed Values
+tf.set_random_seed(789)
+np.random.seed(568)
+
+# -1 Tf activation functions
+def tf_arctan(x):
+    return tf.atan(x)
+def d_tf_arctan(x):
+    return 1.0/(1+tf.square(x))
+
+def tf_ReLU(x):
+    return tf.nn.relu(x)
+def d_tf_ReLu(x):
+    return tf.cast(tf.greater(x, 0),dtype=tf.float32)
+
+def tf_log(x):
+    return tf.sigmoid(x)
+def d_tf_log(x):
+    return tf.sigmoid(x) * (1.0 - tf.sigmoid(x))
+
+# 0. Get the list
+PathDicom = "../lung_data_1/"
+lstFilesDCM1 = []  # create an empty list
+for dirName, subdirList, fileList in sorted(os.walk(PathDicom)):
+    for filename in fileList:
+        if ".jpg" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM1.append(os.path.join(dirName,filename))
+
+# 1. Read the data into Numpy
+one = np.zeros((119,512,512))
+
+# 1.5 Transfer All of the Data into array
+print('===== READING DATA ========')
+for file_index in range(len(lstFilesDCM1)):
+    one[file_index,:,:]   = imread(lstFilesDCM1[file_index],mode='F')
+print('===== Done READING DATA ========')
+
+training_data = one[:100,:,:]
+
+# 1.75 Training Hyper Parameters
+num_epoch = 100
+batch_size = 2
+
+if not os.path.exists('images/'):
+    os.makedirs('images/')
