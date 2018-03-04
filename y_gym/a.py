1b0589c9d33ef1940dc5e1b3bc2e4ed7af7e6d77
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-04 16:16:09 -0500

    's'

--- y_gym/a.py
+++ y_gym/a.py
@@ -1,34 +1,41 @@
-import gym
-env = gym.make('Copy-v0')
-env.reset()
-env.render()
+# import gym
+# env = gym.make('Copy-v0')
+# env.reset()
+# env.render()
+
+# import gym
+# env = gym.make('SpaceInvaders-v0')
+# env.reset()
+# env.render()
+
+# import gym
+# env = gym.make('LunarLander-v2')
+# env.reset()
+# env.render()
+
+# import gym
+# env = gym.make('CartPole-v0')
+# env.reset()
+# env.render()
+
+# import gym
+# env = gym.make('Humanoid-v2')
+# env.reset()
+# env.render()
+
+# import gym
+# env = gym.make('HandManipulateBlock-v0')
+# env.reset()
+# env.render()
 
 import gym
-env = gym.make('SpaceInvaders-v0')
-env.reset()
-env.render()
-
-import gym
-env = gym.make('LunarLander-v2')
-env.reset()
-env.render()
-
-import gym
-env = gym.make('CartPole-v0')
-env.reset()
-env.render()
-
-import gym
+# env = gym.make('FrozenLake-v0')
 env = gym.make('Humanoid-v2')
+
 env.reset()
 env.render()
 
-import gym
-env = gym.make('HandManipulateBlock-v0')
-env.reset()
-env.render()
-
-import gym
-env = gym.make('FrozenLake-v0')
-env.reset()
-env.render()
+for _ in range(1000000): # run for 1000 steps
+    action = env.action_space.sample() # pick a random action
+    env.step(action) # take action
+    env.render()
