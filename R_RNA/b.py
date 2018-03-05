b18c8e4f89e82c642f9b3f4d56c1850da77745f0
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-05 17:58:09 -0500

    'adsa'

--- R_RNA/b.py
+++ R_RNA/b.py
@@ -6,6 +6,15 @@ import numpy as np
 np.random.seed(678)
 tf.set_random_seed(768)
 
+
+def tf_log(x):
+    return tf.sigmoid(x)
+def d_tf_log(x):
+    return tf_log(x) * (1.0 - tf_log(x))
+
+
+
+
 # -1. DNA codon table
 protein_table = {"TTT" : "F", "CTT" : "L", "ATT" : "I", "GTT" : "V",
            "TTC" : "F", "CTC" : "L", "ATC" : "I", "GTC" : "V",
@@ -42,9 +51,6 @@ for training_index in range(num_training_data):
         dna,protein = random.choice(list(protein_table.items()))
         current_data = np.append(current_data,[dna])
         protein_sequence = np.append(protein_sequence,[protein])
-        
-        # current_data = current_data + dna
-        # protein_sequence = protein_sequence + protein_data
 
     if training_index == 0 :
         dna_data = np.expand_dims(current_data,axis=0)
@@ -53,12 +59,23 @@ for training_index in range(num_training_data):
         dna_data = np.vstack((dna_data,np.expand_dims(current_data,axis=0)))
         protein_data = np.vstack((protein_data,np.expand_dims(protein_sequence,axis=0)))
 
+# 1. Create the Model
+class simpleRNN():
 
-print(dna_data)
-print(protein_data)
-    
+    def __init__(self,activation,d_activation):
+
+        self.w = tf.Variable(tf.random_normal([]))
+        self.act = activation
+        self.d_act = d_activation
+
+    def feed_forward(self,input=None):
+
+        self.input = input    
+
+        return self.act(self.input)
 
 
+l1 = simpleRNN(tf_log,d_tf_log)
 
 
 
