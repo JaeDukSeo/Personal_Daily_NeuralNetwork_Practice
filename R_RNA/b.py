4e010c3cd001fee661382f134f2b2363dba5974c
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-05 17:52:39 -0500

    'doe'

--- R_RNA/b.py
+++ R_RNA/b.py
@@ -1,9 +1,62 @@
 import tensorflow as tf
 import random
 import sys
+import numpy as np
+
+np.random.seed(678)
+tf.set_random_seed(768)
+
+# -1. DNA codon table
+protein_table = {"TTT" : "F", "CTT" : "L", "ATT" : "I", "GTT" : "V",
+           "TTC" : "F", "CTC" : "L", "ATC" : "I", "GTC" : "V",
+           "TTA" : "L", "CTA" : "L", "ATA" : "I", "GTA" : "V",
+           "TTG" : "L", "CTG" : "L", "ATG" : "M", "GTG" : "V",
+           "TCT" : "S", "CCT" : "P", "ACT" : "T", "GCT" : "A",
+           "TCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A",
+           "TCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A",
+           "TCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A",
+           "TAT" : "Y", "CAT" : "H", "AAT" : "N", "GAT" : "D",
+           "TAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D",
+            "CAA" : "Q", "AAA" : "K", "GAA" : "E",
+            "CAG" : "Q", "AAG" : "K", "GAG" : "E",
+           "TGT" : "C", "CGT" : "R", "AGT" : "S", "GGT" : "G",
+           "TGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G",
+            "CGA" : "R", "AGA" : "R", "GGA" : "G",
+           "TGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G" 
+           }
+Terminal_Sig = ['TAA','TAG','TGA']
+
+# 0. Generate Random Data of dna to convert to Protein
+num_training_data = 20
+length_of_dna = 9
+
+dna_data = np.array([])
+protein_data = np.array([])
+
+for training_index in range(num_training_data):
+    
+    current_data = np.array([])
+    protein_sequence = np.array([])
+    
+    for _ in range(length_of_dna):
+        dna,protein = random.choice(list(protein_table.items()))
+        current_data = np.append(current_data,[dna])
+        protein_sequence = np.append(protein_sequence,[protein])
+        
+        # current_data = current_data + dna
+        # protein_sequence = protein_sequence + protein_data
+
+    if training_index == 0 :
+        dna_data = np.expand_dims(current_data,axis=0)
+        protein_data = np.expand_dims(protein_sequence,axis=0)
+    else:
+        dna_data = np.vstack((dna_data,np.expand_dims(current_data,axis=0)))
+        protein_data = np.vstack((protein_data,np.expand_dims(protein_sequence,axis=0)))
 
 
-
+print(dna_data)
+print(protein_data)
+    
 
 
 
