6fe3f3ac250fec14ff058f9b7748490885f5db92
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-05 12:38:56 -0500

    's'

--- w_MRI/a.py
+++ w_MRI/a.py
@@ -1,12 +1,35 @@
 import os
 import numpy as np
-from nibabel.testing import data_path
-example_filename = os.path.join(data_path, 'example4d.nii.gz')
+# from nibabel.testing import data_path
+# example_filename = os.path.join(data_path, 'example_nifti2.nii.gz')
+# # zstat1.nii
+# example_filename = os.path.join(data_path, 'example_nifti2.nii.gz')
+import matplotlib.pyplot as plt
 
-
+print(os.path.join(__file__))
 import nibabel as nib
-img = nib.load(example_filename)
+img = nib.load('facemask.nii.gz')
 
+
+
+print(img.shape)
+print(type(img))
+
+data = img.get_data()
+print(data.shape)
+
+for iter in range(data.shape[0]):
+    print(iter)
+    plt.imshow(data[iter,:,:],cmap='gray')
+    plt.pause(0.4)
+
+
+sys.exit()
+
+for iter in range(data.shape[2]):
+    print(iter)
+    plt.imshow(data[:,:,iter],cmap='gray')
+    plt.pause(0.4)
 
 
 # -- end code --
