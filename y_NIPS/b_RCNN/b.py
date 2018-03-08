03dfe8282cf67569faec8138c70844c1c8aa563a
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-08 18:01:23 -0500

    'asd'

--- y_NIPS/b_RCNN/b.py
+++ y_NIPS/b_RCNN/b.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np,sys
 from numpy import float32
 import matplotlib.pyplot as plt
-
+from tensorflow.examples.tutorials.mnist import input_data
 np.random.seed(678)
 tf.set_random_seed(678)
 
@@ -50,13 +50,12 @@ class RCNN():
     def feedforward(self,input=None,timestamp=None):
         
         self.layer  = tf.nn.conv2d(input,self.w_x,strides=[1,1,1,1],padding='VALID')  + \
-                      tf.nn.conv2d(self.hidden[timestamp,:,:,:,:],
-                          self.w_h,strides=[1,1,1,1],padding='SAME') 
+                      tf.nn.conv2d(self.hidden[timestamp,:,:,:,:], self.w_h,strides=[1,1,1,1],padding='SAME') 
         self.layerA = self.act(self.layer)
 
         assign_hidden = []
-        assign_hidden.append(tf.assign(self.hidden[timestamp+1,:,:,:,:],tf.squeeze(self.layer)))
-        assign_hidden.append(tf.assign(self.hiddenA[timestamp+1,:,:,:,:],tf.squeeze(self.layerA)))
+        assign_hidden.append(tf.assign(self.hidden[timestamp+1,:,:,:,:],self.layer))
+        assign_hidden.append(tf.assign(self.hiddenA[timestamp+1,:,:,:,:],self.layerA))
 
         return self.layerA,assign_hidden
 
@@ -72,47 +71,48 @@ class FNN():
         self.layerA = tf_tanh(self.layer)
         return self.layerA
 
-# ------- Preprocess Data --------
-X,Y,names = unpickle('../../z_CIFAR_data/cifar10batchespy/data_batch_1')
-Y = Y.T
-X = np.reshape(X,(3,32,32,10000)).transpose([3,1,2,0])
-X = (X-X.min(axis=0))/(X.max(axis=0)-X.min(axis=0))
+mnist = input_data.read_data_sets("../../MNIST_data/", one_hot=True)
+testing_images, testing_lables =mnist.test.images,mnist.test.labels
+training_images,training_lables =mnist.train.images,mnist.train.labels
 
-X_train,X_test = X[:8000,:,:,:],X[8000:,:,:,:]
-Y_train,Y_test = Y[:8000,:],Y[8000:,:]
-print(X.shape)
-print(X_train.shape)
-print(X_test.shape)
-print(Y.shape)
-print(Y_train.shape)
-print(Y_test.shape)
+testing_images = np.reshape(testing_images,(10000,28,28,1))
+training_images = np.reshape(training_images,(55000,28,28,1))
+
+# ------- Preprocess Data --------
+# X,Y,names = unpickle('../../z_CIFAR_data/cifar10batchespy/data_batch_1')
+# Y = Y.T
+# X = np.reshape(X,(3,32,32,10000)).transpose([3,1,2,0])
+# X = (X-X.min(axis=0))/(X.max(axis=0)-X.min(axis=0))
+# X_train,X_test = X[:8000,:,:,:],X[8000:,:,:,:]
+# Y_train,Y_test = Y[:8000,:],Y[8000:,:]
 
 
 # --- Hyper Parameter ----
-batch_size = 100
+batch_size = 50
 num_epoch = 1000
 print_size = 10
 
 # ---- Make Objects ----
-l1_RCNN = RCNN(4,30,30,1,2,batch_size,tf_tanh,d_tf_tanh)
-l2_RCNN = RCNN(4,28,28,2,3,batch_size,tf_tanh,d_tf_tanh)
-l3_RCNN = RCNN(4,26,26,3,3,batch_size,tf_tanh,d_tf_tanh)
+l1_RCNN = RCNN(4,26,26,1,1,batch_size,tf_Relu,d_tf_Relu)
+l2_RCNN = RCNN(4,24,24,1,1,batch_size,tf_Relu,d_tf_Relu)
+l3_RCNN = RCNN(4,22,22,1,1,batch_size,tf_Relu,d_tf_Relu)
 
-l4_FNN =  FNN(26*26*3,2048)
+l4_FNN =  FNN(22*22*1,2048)
 l5_FNN =  FNN(2048,1024)
 l6_FNN =  FNN(1024,10)
 
 l1w,l2w,l3w,l4w,l5w,l6w = l1_RCNN.getw(),l2_RCNN.getw(),l3_RCNN.getw(),l4_FNN.getw(),l5_FNN.getw(),l6_FNN.getw()
+# l4w,l5w,l6w = l4_FNN.getw(),l5_FNN.getw(),l6_FNN.getw()
 
 # ---- Make Graph -----
-x = tf.placeholder(shape=[None,32,32,3],dtype=tf.float32)
+x = tf.placeholder(shape=[None,28,28,1],dtype=tf.float32)
 y = tf.placeholder(shape=[None,10],dtype=tf.float32)
 time_stamp = tf.constant(0)
 layer_uodate = []
 
-l1_1,l1_1w = l1_RCNN.feedforward(tf.expand_dims(x[:,:,:,0],axis=3),time_stamp+0)
-l1_2,l1_2w = l1_RCNN.feedforward(tf.expand_dims(x[:,:,:,1],axis=3),time_stamp+1)
-l1_3,l1_3w = l1_RCNN.feedforward(tf.expand_dims(x[:,:,:,2],axis=3),time_stamp+2)
+l1_1,l1_1w = l1_RCNN.feedforward(x,time_stamp+0)
+l1_2,l1_2w = l1_RCNN.feedforward(x,time_stamp+1)
+l1_3,l1_3w = l1_RCNN.feedforward(x,time_stamp+2)
 
 l2_1,l2_1w = l2_RCNN.feedforward(l1_1,time_stamp+0)
 l2_2,l2_2w = l2_RCNN.feedforward(l1_2,time_stamp+1)
@@ -129,7 +129,10 @@ l6_FNN     = l6_FNN.feedforward(l5_FNN)
 
 final_softmax = tf_softmax(l6_FNN)
 cost = tf.reduce_sum( -1 * ( y*tf.log(final_softmax) + (1-y) * tf.log(1-final_softmax ) ) )
-correct_prediction = tf.equal(tf.argmax(final_softmax, 1), tf.argmax(y, 1))
+
+predict_max = tf.argmax(final_softmax, 1)
+gt_max = tf.argmax(y, 1)
+correct_prediction = tf.equal(predict_max, gt_max)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 layer_uodate.append(l1_1w+l1_2w+l1_3w+
@@ -137,7 +140,7 @@ layer_uodate.append(l1_1w+l1_2w+l1_3w+
                     l3_1w+l3_2w+l3_3w)
 
 # --- Auto Train ----
-auto_train = tf.train.AdamOptimizer(learning_rate=0.00001).minimize(cost,var_list=l1w+l2w+l3w+l4w+l5w+l6w)
+auto_train = tf.train.AdamOptimizer(learning_rate=0.01).minimize(cost,var_list=l1w+l2w+l3w+l4w+l5w+l6w)
 
 # --- train sesion ----
 with tf.Session() as sess:
@@ -148,9 +151,9 @@ with tf.Session() as sess:
 
     for iter in range(num_epoch):
         
-        for batch_size_index in range(0,len(X_train),batch_size):
-            current_batch = X_train[batch_size_index:batch_size_index+batch_size,:,:,:]
-            current_batch_label = Y_train[batch_size_index:batch_size_index+batch_size,:]
+        for batch_size_index in range(0,len(training_images),batch_size):
+            current_batch = training_images[batch_size_index:batch_size_index+batch_size,:,:,:]
+            current_batch_label = training_lables[batch_size_index:batch_size_index+batch_size,:]
             
             sess_result = sess.run([cost,accuracy,layer_uodate,auto_train],feed_dict={x:current_batch,y:current_batch_label})
             print("Current Iter : ",iter, " current batch: ",batch_size_index, 
@@ -160,11 +163,13 @@ with tf.Session() as sess:
         if iter % print_size==0:
             print("\n----------")
             print("Current Total Cost: ", total_cost_track)
-            current_batch = X_test[0:0+batch_size,:,:,:]
-            current_batch_label = Y_test[0:0+batch_size,:]
-            
-            sess_result = sess.run([cost,accuracy,layer_uodate],feed_dict={x:current_batch,y:current_batch_label})
-            print('Test Current cost: ', sess_result[0],' Current Acc: ', sess_result[1],end='\n')            
+            current_batch = testing_images[:50,:,:,:]
+            current_batch_label = testing_lables[:50,:]
+            sess_result = sess.run([cost,accuracy,
+            final_softmax,
+            predict_max,gt_max,
+            correct_prediction,layer_uodate],feed_dict={x:current_batch,y:current_batch_label})
+            print('Test Current cost: ', sess_result[0],' Current Acc: ', sess_result[1],end='\n')
             print("----------\n")
             
 
