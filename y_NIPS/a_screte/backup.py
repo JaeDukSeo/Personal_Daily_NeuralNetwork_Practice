da068672bab16b16dc3630503195d567d33635b9
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-08 00:31:47 -0500

    ''s

--- y_NIPS/a_screte/backup.py
+++ y_NIPS/a_screte/backup.py
@@ -49,24 +49,22 @@ class prepnetwork():
         self.w2 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
         self.w3 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
         self.w4 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
-        self.w5 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w5 = tf.Variable(tf.random_normal([3,3,hid_size,1]))
 
-        self.w6 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w7 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w8 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w9 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w10 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
+        self.w6 = tf.Variable(tf.random_normal([3,3,1,hid_size]))
+        self.w7 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w8 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w4 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w5 = tf.Variable(tf.random_normal([3,3,hid_size,1]))
 
-        self.w11 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w12 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w13 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w14 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w15 = tf.Variable(tf.random_normal([5,5,hid_size,1]))
+        self.w1 = tf.Variable(tf.random_normal([3,3,1,hid_size]))
+        self.w2 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w3 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w4 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w5 = tf.Variable(tf.random_normal([3,3,hid_size,1]))
 
 
-    def getw(self): return [self.w1,self.w2,self.w3,self.w4,self.w5,
-                            self.w6,self.w7,self.w8,self.w9,self.w10,
-                            self.w11,self.w12,self.w13,self.w14,self.w15]
+    def getw(self): return [self.w1,self.w2,self.w3,self.w4,self.w5]
 
     def feedforward(self,input=None):
         layer1 = tf.nn.conv2d(input,self.w1,strides=[1,1,1,1],padding='SAME')
@@ -79,55 +77,18 @@ class prepnetwork():
         layer4 = tf_tanh(layer4)
         layer5 = tf.nn.conv2d(layer4,self.w5,strides=[1,1,1,1],padding='SAME')
         layer5 = tf_log(layer5)
-
-        layer6 = tf.nn.conv2d(layer5,self.w6,strides=[1,1,1,1],padding='SAME')
-        layer6 = tf_tanh(layer6)
-        layer7 = tf.nn.conv2d(layer6,self.w7,strides=[1,1,1,1],padding='SAME')
-        layer7 = tf_tanh(layer7)
-        layer8 = tf.nn.conv2d(layer7,self.w8,strides=[1,1,1,1],padding='SAME')
-        layer8 = tf_tanh(layer8)
-        layer9 = tf.nn.conv2d(layer8,self.w9,strides=[1,1,1,1],padding='SAME')
-        layer9 = tf_tanh(layer9)
-        layer10 = tf.nn.conv2d(layer9,self.w10,strides=[1,1,1,1],padding='SAME')
-        layer10 = tf_log(layer10)
-
-        layer11 = tf.nn.conv2d(layer10,self.w11,strides=[1,1,1,1],padding='SAME')
-        layer11 = tf_tanh(layer11)
-        layer12 = tf.nn.conv2d(layer11,self.w12,strides=[1,1,1,1],padding='SAME')
-        layer12 = tf_tanh(layer12)
-        layer13 = tf.nn.conv2d(layer12,self.w13,strides=[1,1,1,1],padding='SAME')
-        layer13 = tf_tanh(layer13)
-        layer14 = tf.nn.conv2d(layer13,self.w14,strides=[1,1,1,1],padding='SAME')
-        layer14 = tf_tanh(layer14)
-        layer15 = tf.nn.conv2d(layer14,self.w15,strides=[1,1,1,1],padding='SAME')
-        layer15 = tf_log(layer15)
-        return layer15
+        return layer5
 
 class hiddingnetwork():
     
     def __init__(self,hid_size=None):
-        self.w1 = tf.Variable(tf.random_normal([3,3,1,hid_size]))
-        self.w2 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
-        self.w3 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
-        self.w4 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
-        self.w5 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w1 = tf.Variable(tf.random_normal([4,4,1,hid_size]))
+        self.w2 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
+        self.w3 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
+        self.w4 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
+        self.w5 = tf.Variable(tf.random_normal([4,4,hid_size,1]))
 
-        self.w6 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w7 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w8 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w9 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w10 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-
-        self.w11 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w12 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w13 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w14 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w15 = tf.Variable(tf.random_normal([5,5,hid_size,1]))
-
-
-    def getw(self): return [self.w1,self.w2,self.w3,self.w4,self.w5,
-                            self.w6,self.w7,self.w8,self.w9,self.w10,
-                            self.w11,self.w12,self.w13,self.w14,self.w15]
+    def getw(self): return [self.w1,self.w2,self.w3,self.w4,self.w5]
 
     def feedforward(self,input=None):
         layer1 = tf.nn.conv2d(input,self.w1,strides=[1,1,1,1],padding='SAME')
@@ -140,55 +101,18 @@ class hiddingnetwork():
         layer4 = tf_tanh(layer4)
         layer5 = tf.nn.conv2d(layer4,self.w5,strides=[1,1,1,1],padding='SAME')
         layer5 = tf_log(layer5)
-
-        layer6 = tf.nn.conv2d(layer5,self.w6,strides=[1,1,1,1],padding='SAME')
-        layer6 = tf_tanh(layer6)
-        layer7 = tf.nn.conv2d(layer6,self.w7,strides=[1,1,1,1],padding='SAME')
-        layer7 = tf_tanh(layer7)
-        layer8 = tf.nn.conv2d(layer7,self.w8,strides=[1,1,1,1],padding='SAME')
-        layer8 = tf_tanh(layer8)
-        layer9 = tf.nn.conv2d(layer8,self.w9,strides=[1,1,1,1],padding='SAME')
-        layer9 = tf_tanh(layer9)
-        layer10 = tf.nn.conv2d(layer9,self.w10,strides=[1,1,1,1],padding='SAME')
-        layer10 = tf_log(layer10)
-
-        layer11 = tf.nn.conv2d(layer10,self.w11,strides=[1,1,1,1],padding='SAME')
-        layer11 = tf_tanh(layer11)
-        layer12 = tf.nn.conv2d(layer11,self.w12,strides=[1,1,1,1],padding='SAME')
-        layer12 = tf_tanh(layer12)
-        layer13 = tf.nn.conv2d(layer12,self.w13,strides=[1,1,1,1],padding='SAME')
-        layer13 = tf_tanh(layer13)
-        layer14 = tf.nn.conv2d(layer13,self.w14,strides=[1,1,1,1],padding='SAME')
-        layer14 = tf_tanh(layer14)
-        layer15 = tf.nn.conv2d(layer14,self.w15,strides=[1,1,1,1],padding='SAME')
-        layer15 = tf_log(layer15)
-        return layer15
+        return layer5
 
 class revealnetwork():
     
     def __init__(self,hid_size=None):
-        self.w1 = tf.Variable(tf.random_normal([3,3,1,hid_size]))
-        self.w2 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
-        self.w3 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
-        self.w4 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
-        self.w5 = tf.Variable(tf.random_normal([3,3,hid_size,hid_size]))
+        self.w1 = tf.Variable(tf.random_normal([5,5,1,hid_size]))
+        self.w2 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
+        self.w3 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
+        self.w4 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
+        self.w5 = tf.Variable(tf.random_normal([5,5,hid_size,1]))
 
-        self.w6 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w7 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w8 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w9 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-        self.w10 = tf.Variable(tf.random_normal([4,4,hid_size,hid_size]))
-
-        self.w11 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w12 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w13 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w14 = tf.Variable(tf.random_normal([5,5,hid_size,hid_size]))
-        self.w15 = tf.Variable(tf.random_normal([5,5,hid_size,1]))
-
-
-    def getw(self): return [self.w1,self.w2,self.w3,self.w4,self.w5,
-                            self.w6,self.w7,self.w8,self.w9,self.w10,
-                            self.w11,self.w12,self.w13,self.w14,self.w15]
+    def getw(self): return [self.w1,self.w2,self.w3,self.w4,self.w5]
 
     def feedforward(self,input=None):
         layer1 = tf.nn.conv2d(input,self.w1,strides=[1,1,1,1],padding='SAME')
@@ -196,34 +120,12 @@ class revealnetwork():
         layer2 = tf.nn.conv2d(layer1,self.w2,strides=[1,1,1,1],padding='SAME')
         layer2 = tf_tanh(layer2)
         layer3 = tf.nn.conv2d(layer2,self.w3,strides=[1,1,1,1],padding='SAME')
-        layer3 = tf_tanh(layer3)
+        layer3 = tf_log(layer3)
         layer4 = tf.nn.conv2d(layer3,self.w4,strides=[1,1,1,1],padding='SAME')
         layer4 = tf_tanh(layer4)
         layer5 = tf.nn.conv2d(layer4,self.w5,strides=[1,1,1,1],padding='SAME')
         layer5 = tf_log(layer5)
-
-        layer6 = tf.nn.conv2d(layer5,self.w6,strides=[1,1,1,1],padding='SAME')
-        layer6 = tf_tanh(layer6)
-        layer7 = tf.nn.conv2d(layer6,self.w7,strides=[1,1,1,1],padding='SAME')
-        layer7 = tf_tanh(layer7)
-        layer8 = tf.nn.conv2d(layer7,self.w8,strides=[1,1,1,1],padding='SAME')
-        layer8 = tf_tanh(layer8)
-        layer9 = tf.nn.conv2d(layer8,self.w9,strides=[1,1,1,1],padding='SAME')
-        layer9 = tf_tanh(layer9)
-        layer10 = tf.nn.conv2d(layer9,self.w10,strides=[1,1,1,1],padding='SAME')
-        layer10 = tf_log(layer10)
-
-        layer11 = tf.nn.conv2d(layer10,self.w11,strides=[1,1,1,1],padding='SAME')
-        layer11 = tf_tanh(layer11)
-        layer12 = tf.nn.conv2d(layer11,self.w12,strides=[1,1,1,1],padding='SAME')
-        layer12 = tf_tanh(layer12)
-        layer13 = tf.nn.conv2d(layer12,self.w13,strides=[1,1,1,1],padding='SAME')
-        layer13 = tf_tanh(layer13)
-        layer14 = tf.nn.conv2d(layer13,self.w14,strides=[1,1,1,1],padding='SAME')
-        layer14 = tf_tanh(layer14)
-        layer15 = tf.nn.conv2d(layer14,self.w15,strides=[1,1,1,1],padding='SAME')
-        layer15 = tf_log(layer15)
-        return layer15
+        return layer5
 
 # ------- Preprocess Data --------
 X,Y,names = unpickle('../../z_CIFAR_data/cifar10batchespy/data_batch_1')
@@ -241,7 +143,7 @@ print('-----------------')
 # c_images = X[1000:2000,:,:,:]
 
 # Declare the Objects and the networks, and get the weigths for auto
-hid_size = 10
+hid_size = 20
 print_size = 5
 learing_rate = 0.01
 batch_size = 10
@@ -250,7 +152,7 @@ beta_rate_for_s = 0.8
 # Declare the Object and get the weights
 prepnetwork = prepnetwork(hid_size)
 hididingnetwork = hiddingnetwork(hid_size)
-revealnetwork = revealnetwork(hid_size)
+revealnetwork = revealnetwork(hid_size+5)
 prepw,hidw,revw = prepnetwork.getw(),hididingnetwork.getw(),revealnetwork.getw()
 
 #==================================================
