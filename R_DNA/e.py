d42b6f76ab154db9b124824a8739034137af7f6c
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-09 21:45:07 -0500

    ;

--- R_DNA/e.py
+++ R_DNA/e.py
@@ -8,8 +8,17 @@ from sklearn.preprocessing import LabelEncoder
 np.random.seed(678)
 tf.set_random_seed(768)
 
+# Activation Functions - however there was no indication in the original paper
+def tf_log(x): 
+    return tf.sigmoid(x)
 
+def d_tf_log(x): 
+    return tf.multiply(tf_log(x),tf.subtract(1.0,tf_log(x))) 
 
+def tf_tanh(x): return tf.tanh(x)
+def d_tf_tanh(x): return 1.0 - tf.square(tf_tanh(x))
+
+def tf_softmax(x): return tf.nn.softmax(x)
 
 # -1. DNA encode table
 protein_table = {"TTT" : "F", "CTT" : "L", "ATT" : "I", "GTT" : "V",
@@ -31,9 +40,16 @@ protein_table = {"TTT" : "F", "CTT" : "L", "ATT" : "I", "GTT" : "V",
            }
 
 # 0. Generate Random Data of dna to convert to Protein
-num_training_data = 500
+num_training_data = 1000
 length_of_protein = 1
-print_interval = 3
+
+learning_rate = 0.0005
+
+beta_1 ,beta_2= 0.9, 0.999
+adam_e = 0.00000001
+
+num_epoch = 800
+print_size = 50
 
 # 1. Array to Contain all of the data 
 dna_data = np.array([])
@@ -65,7 +81,6 @@ for training_index in range(num_training_data):
     else:
         dna_data = np.vstack((dna_data,np.expand_dims(current_data,axis=0)))
         protein_data = np.vstack((protein_data,np.expand_dims(protein_sequence,axis=0)))
-
         
 label_encoder = LabelEncoder()
 integer_encoded = label_encoder.fit_transform(protein_data)
@@ -74,23 +89,106 @@ integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
 protein_data = onehot_encoder.fit_transform(integer_encoded)
 
 print('------ One Hot Encoded Protein Data-----')
-print(protein_data.shape)
 print(dna_data.shape)
+print(protein_data.shape)
 
 # 2. Make the class
 class FCNN:
     
     def __init__(self,input=None,output=None,act=None,d_act=None):
-        
+
         self.w = tf.Variable(tf.random_normal([input,output]))
-        self.act = act
-        self.d_act = d_act
+
+        self.activation = act
+        self.d_activation = d_act
+        self.m,self.v = tf.Variable(tf.zeros_like(self.w)),tf.Variable(tf.zeros_like(self.w))
+
+        self.input,self.layer,self.layerA= None,None,None
+
+    def getw(self): return self.w
 
     def feedforward(self,input=None):
+        self.input = input
+        self.layer  = tf.matmul(self.input,self.w)
+        self.layerA = self.activation(self.layer)
+        return self.layerA
+    
+    def backprop(self,gradient=None):
         
-        self.layer = tf.matmul(input,self.w)
+        grad_part_1 = gradient
+        grad_part_2 = self.d_activation(self.layer) 
+        grad_part_3 = self.input
 
-layer1 = FCNN(12,100)
+        grad = tf.matmul(tf.transpose(grad_part_3),tf.multiply(grad_part_1,grad_part_2))
+        grad_pass = tf.matmul(tf.multiply(grad_part_1,grad_part_2),tf.transpose(self.w))
+
+        assign = []
+        assign.append(tf.assign(self.m,beta_1*self.m + (1.0-beta_1)*grad))
+        assign.append(tf.assign(self.v,beta_2*self.v + (1.0-beta_2)*tf.square(grad) ))    
+
+        m_hat = tf.divide(self.m, tf.subtract(1.0,beta_1) )
+        v_hat = tf.divide(self.v, tf.subtract(1.0,beta_2) )
+
+        adam_middle = tf.divide(learning_rate,tf.add(tf.sqrt(v_hat),adam_e))
+        assign.append(  tf.assign(self.w, tf.subtract(self.w,tf.multiply(adam_middle,m_hat)))  )
+
+        return grad_pass,assign
+
+# 3.Make the Layers
+layer1 = FCNN(12,48,tf_log,d_tf_log)
+layer2 = FCNN(48,75,tf_log,d_tf_log)
+layer3 = FCNN(75,140,tf_log,d_tf_log)
+layer4 = FCNN(140,20,tf_log,d_tf_log)
+
+l1w,l2w,l3w,l4w = layer1.getw(),layer2.getw(),layer3.getw(),layer4.getw()
+
+# 4. Make the graph
+x = tf.placeholder(shape=[None,12],dtype=tf.float32)
+y = tf.placeholder(shape=[None,20],dtype=tf.float32)
+decay_rate = 
+
+l1 = layer1.feedforward(x)
+l2 = layer2.feedforward(l1)
+l3 = layer3.feedforward(l2)
+l4 = layer4.feedforward(l3)
+final_soft = tf_softmax(l4)
+
+cost = tf.reduce_sum(-1.0 * (y* tf.log(final_soft) + (1-y)*tf.log(1-final_soft)))
+correct_prediction = tf.equal(tf.argmax(final_soft, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# --- Auto Train ----
+auto_train= tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost,var_list=[l1w,l2w,l3w,l4w])
+
+l4g,l4update = layer4.backprop(final_soft - y)
+l3g,l3update = layer3.backprop(l4g)
+l2g,l2update = layer2.backprop(l3g)
+l1g,l1update = layer1.backprop(l2g)
+weight_updates = l4update + l3update + l2update + l1update
+
+# 5. Train
+with tf.Session() as sess:
+
+    sess.run(tf.global_variables_initializer())
+
+    for iter in range(num_epoch):
+        
+        for batch_index in range(0,len(protein_data),100):
+            
+            current_batch = dna_data[batch_index:batch_index+100,:]
+            current_batch_label = protein_data[batch_index:batch_index+100,:]
+
+            # sess_results = sess.run([accuracy,auto_train],feed_dict={x:current_batch,y:current_batch_label})
+            sess_results = sess.run([accuracy,cost,weight_updates,correct_prediction],feed_dict={x:current_batch,y:current_batch_label})
+            
+            print("Current Iter: ", iter, " Current acuuracy: ", sess_results[0], " current cost: ",sess_results[1], end='\r')
+
+        if iter %print_size==0:
+            print('\n')
+            
+
+
+
 
 
 # -- end code --
---
+++ R_DNA/refference.py
@@ -0,0 +1,244 @@
+
+import numpy as np,sys
+from sklearn.utils import shuffle
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import matplotlib.pyplot as plt
+np.random.seed(678)
+tf.set_random_seed(678)
+
+def tf_log(x):
+    return tf.div(tf.constant(1.0),tf.add(tf.constant(1.0), tf.exp(tf.negative(x))))
+def d_tf_log(x):
+    return tf.multiply(tf_log(x),tf.subtract(  tf.constant(1.0), tf_log(x))  )
+
+def tf_tanh(x):
+    return tf.tanh(x)
+def d_tf_tanh(x):
+    return tf.subtract(tf.constant(1.0),tf.square(tf.tanh(x)))
+
+
+def tf_arctan(x):
+    return tf.atan(x)
+def d_tf_arctan(x):
+    return tf.div(tf.constant(1.0),tf.subtract( tf.constant(1.0) , tf.square(x) ))
+
+
+# 0. Declare Training Data and Labels
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
+
+train = mnist.test
+images, labels = train.images, train.labels
+only_zero_index,only_one_index = np.where(labels==0)[0],np.where(labels==1)[0]
+only_zero_image,only_zero_label = images[[only_zero_index]],np.expand_dims(labels[[only_zero_index]],axis=1)
+only_one_image,only_one_label   = images[[only_one_index]],np.expand_dims(labels[[only_one_index]],axis=1)
+
+images = np.vstack((only_zero_image,only_one_image))
+labels = np.vstack((only_zero_label,only_one_label))
+images,label = shuffle(images,labels)
+
+test_image_num,training_image_num = 20,100
+testing_images, testing_lables =images[:test_image_num,:],label[:test_image_num,:]
+training_images,training_lables =images[test_image_num:test_image_num + training_image_num,:],label[test_image_num:test_image_num + training_image_num,:]
+
+num_epoch = 100
+total_cost = 0
+cost_array =[]
+graph = tf.Graph()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 1. Think What weights do I need? And how to initialize them 
+with graph.as_default():
+
+    lr_x = tf.Variable(tf.constant(0.001))
+    lr_rec = tf.Variable(tf.constant(0.000001))
+    lr_sg = tf.Variable(tf.constant(0.0001))
+    
+    hidden_states = tf.Variable(tf.random_normal([784,3]))
+
+    w_x = tf.Variable(tf.random_normal([784,784],stddev=0.45) *  tf.constant(0.2)) 
+    w_rec = tf.Variable(tf.random_normal([784,784],stddev=0.035) *  tf.constant(0.2))
+    w_fc = tf.Variable(tf.random_normal([784,1],stddev=0.95)*  tf.constant(0.2))
+
+    w_sg_1 = tf.Variable(tf.random_normal([784,784],stddev=0.35)*  tf.constant(0.2))
+    w_sg_2 = tf.Variable(tf.random_normal([784,784],stddev=0.35)*  tf.constant(0.2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2. Think What is the structure of this Network? How does it work - EVEN BACK PROP!
+with graph.as_default():
+
+    x = tf.placeholder(tf.float32, [None, 784])
+    y = tf.placeholder(tf.float32, [None, 1])
+    update = []
+    h_update = []
+
+    l1 = tf.add(tf.matmul(x,w_x),tf.matmul(tf.expand_dims(hidden_states[:,0],axis=0), w_rec  )  )
+    l1A = tf_tanh(l1)  
+    h_update.append(tf.assign(hidden_states[:,1],tf.squeeze(l1A) ))
+
+    # # ----- Time Stamp 1 Syn Grad Update ------
+    grad_1sg_part_1 = tf.matmul(l1A,w_sg_1)
+    grad_1sg_part_2 = d_tf_tanh(l1)
+    grad_1sg_part_rec = tf.expand_dims(hidden_states[:,0],axis=0)
+    grad_1sg_part_x = x
+    
+    grad_1sg_rec = tf.matmul(tf.transpose(grad_1sg_part_rec),tf.multiply( grad_1sg_part_1,grad_1sg_part_2 ) )
+    grad_1sg_x =   tf.matmul(tf.transpose(grad_1sg_part_x),  tf.multiply( grad_1sg_part_1,grad_1sg_part_2 ) )
+    
+    update.append(tf.assign(w_rec, tf.add(w_rec, tf.multiply(lr_rec,grad_1sg_rec)))  )
+    update.append(tf.assign(w_x,   tf.add(w_x,   tf.multiply(lr_rec,grad_1sg_x)))   )
+    grad_true_0 = tf.matmul(tf.multiply(grad_1sg_part_1,grad_1sg_part_2),tf.transpose(w_rec))
+    # # ----- Time Stamp 1 Syn Grad Update ------
+
+    l2 = tf.add(tf.matmul(x,w_x),tf.matmul(tf.expand_dims(hidden_states[:,1],axis=0),w_rec)    )  
+    l2A = tf_tanh(l2)  
+    h_update.append(tf.assign(hidden_states[:,2], tf.squeeze(l2A) ))
+     
+    # # ----- Time Stamp 2 Syn Grad Update ------
+    grad_2sg_part_1 = tf.matmul(l2A,w_sg_2)
+    grad_2sg_part_2 = d_tf_tanh(l2)
+    grad_2sg_part_rec = tf.expand_dims(hidden_states[:,1],axis=0)
+    grad_2sg_part_x =  x
+    
+    grad_2sg_rec = tf.matmul(tf.transpose(grad_2sg_part_rec),tf.multiply( grad_2sg_part_1,grad_2sg_part_2 ) )
+    grad_2sg_x =   tf.matmul(tf.transpose(grad_2sg_part_x),  tf.multiply( grad_2sg_part_1,grad_2sg_part_2 ) )
+    
+    update.append(tf.assign(w_rec, tf.add(w_rec, tf.multiply(lr_rec,grad_2sg_rec)))   )
+    update.append(tf.assign(w_x,   tf.add(w_x,   tf.multiply(lr_rec,grad_2sg_x)))   )
+    grad_true_1_from_2 = tf.matmul(tf.multiply(grad_2sg_part_1,grad_2sg_part_2),tf.transpose(w_rec))
+    # # ----- Time Stamp 2 Syn Grad Update ------
+
+    # # ----- Time Stamp 1 True Gradient Update ------
+    grad_true_1_part_1 = tf.subtract(grad_1sg_part_1,grad_true_1_from_2)
+    grad_true_1_part_2 = tf.expand_dims(hidden_states[:,1],axis=0)
+    grad_true_1 = tf.matmul(tf.transpose(grad_true_1_part_2),grad_true_1_part_1)
+    update.append(tf.assign(w_sg_1,tf.subtract(w_sg_1,tf.multiply(lr_sg,grad_true_1))))
+    # # ----- Time Stamp 1 True Gradient Update ------
+
+    # # ----- Fully Connected for Classification ------
+    l3 = tf.matmul( tf.expand_dims(hidden_states[:,2],axis=0),w_fc)
+    l3A = tf_log(l3)
+    # # ----------------------------------------------
+
+    # # -- MAN BACK PROP -----
+    cost = tf.multiply(tf.square(tf.subtract(l3A,y)),tf.constant(0.5))
+    # # -- MAN BACK PROP -----
+    
+    # # -- AUTO BACK PROP -----
+    cost_auto = tf.train.GradientDescentOptimizer(0.1).minimize(cost)
+    # # -- AUTO BACK PROP -----
+
+    # # ------- FC weight update ---------------------
+    grad_fc_part_1 = tf.subtract(l3A,y)
+    grad_fc_part_2 = d_tf_log(l3)
+    grad_fc_part_3 = tf.expand_dims(hidden_states[:,2],axis=0)
+    grad_fc = tf.matmul(tf.transpose(grad_fc_part_3),tf.multiply(grad_fc_part_1,grad_fc_part_2))
+    update.append(tf.assign(w_fc,tf.subtract(w_fc,tf.multiply(lr_x,grad_fc))))
+
+    grad_true_2_from_3 = tf.matmul(tf.multiply(grad_fc_part_1,grad_fc_part_2),tf.transpose(w_fc))
+    # # ------- FC weight update ---------------------
+        
+    # # ----- Time Stamp 2 True Gradient Update ------
+    grad_true_2_part_1 = tf.subtract(grad_2sg_part_1,grad_true_2_from_3)
+    grad_true_2_part_2 = tf.expand_dims(hidden_states[:,2],axis=0)
+    grad_true_2 = tf.matmul(tf.transpose(grad_true_2_part_2),grad_true_2_part_1)
+    update.append(tf.assign(w_sg_2,tf.subtract(w_sg_2,tf.multiply(lr_sg,grad_true_2))) ) 
+    # # ----- Time Stamp 2 True Gradient Update ------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3. Run the Session, however think about how many batches etc
+with tf.Session(graph=graph) as sess:
+
+    sess.run(tf.global_variables_initializer())
+    total_cost = 0 
+
+    for iter in range(num_epoch):
+        for current_image_index in range(len(training_images)):
+            
+            current_image = np.expand_dims(training_images[current_image_index],axis=0)
+            current_index = np.expand_dims(training_lables[current_image_index],axis=0)
+            
+            # If you wish to do manual back prop
+            output = sess.run([cost,update,h_update],feed_dict={x:current_image,y:current_index})
+
+            # If you wish to use the auto differential
+#             output = sess.run([cost,cost_auto,h_update],feed_dict={x:current_image,y:current_index})
+
+            total_cost = total_cost + output[0].sum()
+        print("Current Iter: ", iter, " current cost: ", total_cost)
+        cost_array.append(total_cost)
+        total_cost = 0
+
+    
+    plt.plot(np.arange(num_epoch), cost_array)
+    plt.show()
+
+
+    for current_image_index in range(len(testing_images)):
+        current_image = np.expand_dims(testing_images[current_image_index],axis=0)
+        current_label = testing_lables[current_image_index]
+        output = sess.run([l3A,h_update],feed_dict={x:current_image})
+        print(current_image_index,' : ' ,output[0], ' : ',np.round(output[0])," : ",current_label)
+
+
+
+
+
+
+
+
+
+
+
+
+# ---- end code ---
