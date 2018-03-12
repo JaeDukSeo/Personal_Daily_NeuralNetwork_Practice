0c4d897e9534fcbb73c766d011cc082ba4a20cd3
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-11 21:20:33 -0400

    ''s

--- y_ICR/dilated.py
+++ y_ICR/dilated.py
@@ -32,17 +32,42 @@ print(np_results)
 
 # 3. TF convolution
 print("========== Tensorfow Results ===============")
-tf_operation = tf.nn.conv2d(np.expand_dims(np.expand_dims(matrix,axis=0),axis=3),
+tf_operation1 = tf.nn.conv2d(np.expand_dims(np.expand_dims(matrix,axis=0),axis=3),
                             np.expand_dims(np.expand_dims(kernel,axis=3),axis=4),
                             strides=[1,1,1,1],padding="VALID")
-tf_result = sess.run(tf_operation)
+tf_result = sess.run(tf_operation1)
 print(tf_result.shape)
 print(np.squeeze(tf_result))
 
 
 # 4. 
 
-# 5.
+# 5. TF Dilated with Dilation of 1
+print("========== Tensorfow Results Dilated Value of 1 ===============")
+tf_opreation2 = tf.nn.atrous_conv2d(np.expand_dims(np.expand_dims(matrix,axis=0),axis=3),
+                                    np.expand_dims(np.expand_dims(kernel,axis=3),axis=4),
+                                    rate=1,padding="VALID")
+tf_result = sess.run(tf_opreation2)
+print(tf_result.shape)
+print(np.squeeze(tf_result))
+
+# 6. TF Dilated with Dilation of 2
+print("========== Tensorfow Results Dilated Value of 2 ===============")
+tf_opreation3 = tf.nn.atrous_conv2d(np.expand_dims(np.expand_dims(matrix,axis=0),axis=3),
+                                    np.expand_dims(np.expand_dims(kernel,axis=3),axis=4),
+                                    rate=2,padding="VALID")
+tf_result = sess.run(tf_opreation3)
+print(tf_result.shape)
+print(np.squeeze(tf_result))
+
+# 7. TF Dilated with Dilation of 3
+print("========== Tensorfow Results Dilated Value of 3 ===============")
+tf_opreation4 = tf.nn.atrous_conv2d(np.expand_dims(np.expand_dims(matrix,axis=0),axis=3),
+                                    np.expand_dims(np.expand_dims(kernel,axis=3),axis=4),
+                                    rate=3,padding="VALID")
+tf_result = sess.run(tf_opreation4)
+print(tf_result.shape)
+print(np.squeeze(tf_result))
 
 
 # -- end code --
