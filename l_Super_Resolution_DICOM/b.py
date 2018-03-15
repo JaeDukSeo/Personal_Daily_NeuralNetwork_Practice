dbfc9454c17de4d8b9f688290c16bc433bbb4d24
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-15 09:18:37 -0400

    's'

--- l_Super_Resolution_DICOM/b.py
+++ l_Super_Resolution_DICOM/b.py
@@ -4,8 +4,7 @@ from scipy import misc
 import dicom
 import matplotlib.pyplot as plt
 from skimage.measure import compare_psnr
-from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral,
-                                 denoise_wavelet, estimate_sigma)
+from sklearn.utils import shuffle
 import shutil
 np.random.seed(678)
 tf.set_random_seed(678)
@@ -60,7 +59,7 @@ class FCNN():
         self.act,self.d_act = act,d_act
         self.m,self.v = tf.Variable(tf.zeros_like(self.w)),tf.Variable(tf.zeros_like(self.w))
 
-    def getw(self): return [self.w,self.b ]
+    def getw(self): return [self.w,self.b]
 
     def feedforward(self,input,stride_row,stride_col,padding,og_input= None,x_input=None):
         self.input  = input
@@ -70,10 +69,12 @@ class FCNN():
         else:
             self.layer  = tf.nn.conv2d(input*og_input,self.w,strides=[1,stride_row,stride_col,1],padding=padding) + self.b 
 
+        self.layer = tf.nn.dropout(self.layer,0.5)
+
         if  x_input==None:
             self.layerA = self.act(self.layer)
         else:            
-            self.layerA = (self.act(self.layer)+tf_blur(x_input) ) * tf_small_blur(x_input)
+            self.layerA = (self.act(self.layer)*tf_small_blur(x_input) ) + tf_small_blur(x_input)
         return self.layerA
 
     def backprop(self,gradient=None,og_input=None):
@@ -133,25 +134,35 @@ for file_index in range(len(lstFilesDCM_low)):
     high_dose[file_index,:,:,:] = temp
 print('===== READING / NORMALIZING DATA ========')
 
+
+low_dose_train_og = low_dose[:244+82,:,:,:]
+high_dose_train_og = high_dose[:244+82,:,:,:]
+
+# low_dose_cv = low_dose[244:244+82,:,:,:]
+# high_dose_cv = high_dose[244:244+82,:,:,:]
+
+low_dose_test = low_dose[244+82:,:,:,:]
+high_dose_test = high_dose[244+82:,:,:,:]
+
 # Save Location
-save_location = "./images1/"
+save_location = "./images_test/"
 if os.path.exists(save_location):
     shutil.rmtree(save_location)
 os.makedirs(save_location)
 
 # Hyper Param
-learning_rate = 0.005 
-num_epoch = 101
+learning_rate = 0.05 
+num_epoch = 301
 batch_size = 10 
 print_size= 10
 
 # Make Layer Object
-l1 = FCNN(6,1,13,tf_ReLU,d_tf_tanh)
-l2 = FCNN(3,13,13,tf_ReLU,d_tf_tanh)
-l3 = FCNN(2,13,13,tf_ReLU,d_tf_tanh)
-l4 = FCNN(1,9,1,tf_log,d_tf_tanh)
+l1 = FCNN(6,1,3,tf_ReLU,d_tf_tanh)
+l2 = FCNN(3,3,3,tf_ReLU,d_tf_tanh)
+l3 = FCNN(2,6,3,tf_ReLU,d_tf_tanh)
+l4 = FCNN(1,6,3,tf_ReLU,d_tf_tanh)
+l5 = FCNN(1,3,1,tf_log,d_tf_tanh)
 
-l5 = FCNN(2,3,3,tf_tanh,d_tf_tanh)
 l6 = FCNN(2,3,3,tf_ReLU,d_tf_tanh)
 l7 = FCNN(1,3,1,tf_log,d_tf_ReLu)
 
@@ -164,15 +175,19 @@ y = tf.placeholder(shape=[None,512,512,1],dtype=tf.float32)
 
 layer1 = l1.feedforward(x,1,1,"SAME",x_input=x)
 layer2 = l2.feedforward(layer1,1,1,"SAME",og_input=x,x_input=x)
-layer3 = l3.feedforward(layer2,1,1,"SAME",og_input=layer1*x,x_input=x)
-layer4 = l4.feedforward(layer3,1,1,"SAME",og_input=layer1*layer2*x,x_input=x)
-# layer5 = l5.feedforward(layer4,1,1,"SAME",layer1*layer2*layer3,x)
+
+layer3_Input = tf.concat((layer1,layer2),axis=3)
+layer3 = l3.feedforward(layer3_Input,1,1,"SAME",og_input=None,x_input=x)
+
+layer4_Input = tf.concat((layer2,layer3),axis=3)
+layer4 = l4.feedforward(layer4_Input,1,1,"SAME",og_input=None,x_input=x)
+layer5 = l5.feedforward(layer4,1,1,"SAME",og_input=layer1*layer2*layer3*x,x_input=x)
 # layer6 = l6.feedforward(layer5,1,1,"SAME",layer1*layer2*layer3*layer4,x)
 # layer7 = l7.feedforward(layer6,1,1,"SAME",layer1*layer2*layer3*layer4*layer5,x)
 
-cost = tf.square(tf.subtract(layer4,y))
+cost = tf.square(tf.subtract(layer5,y))
 cost_PSNR = 10*log10(255.0/tf.sqrt(tf.reduce_mean(cost)))
-auto_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost,var_list=[l1w+l2w+l3w+l4w])
+auto_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost,var_list=[l1w+l2w+l3w+l4w+l5w])
 
 with tf.Session() as sess:
 
@@ -182,49 +197,60 @@ with tf.Session() as sess:
 
     for iter in range(num_epoch):
         
-        for current_batch_index in range(0,len(low_dose),batch_size):
-            current_batch_low  = low_dose[current_batch_index:current_batch_index+batch_size,:,:,:]
-            current_batch_high = high_dose[current_batch_index:current_batch_index+batch_size,:,:,:]
+        low_dose_train,high_dose_train = shuffle(low_dose_train_og,high_dose_train_og)
+        low_dose_cv =    low_dose_train[:82,:,:,:]
+        high_dose_cv =    high_dose_train[:82,:,:,:]
+        
+        low_dose_train = low_dose_train[82:,:,:,:]
+        high_dose_train = high_dose_train[82:,:,:,:]
+        
+        for current_batch_index in range(0,len(low_dose_train),batch_size):
+            current_batch_low  = low_dose_train[current_batch_index:current_batch_index+batch_size,:,:,:]
+            current_batch_high = high_dose_train[current_batch_index:current_batch_index+batch_size,:,:,:]
             sess_result = sess.run([cost,auto_train],feed_dict={x:current_batch_low,y:current_batch_high})
             print("Current Iter: ", iter, " current batch: ", current_batch_index, " current Cost : ", sess_result[0].sum(),end='\r')
             total_cost= total_cost + np.sum(sess_result[0])
 
         if iter%print_size==0:
-            print("\n=====================")
-            for sample in range(0,408,50):
-                current_batch_low  = np.expand_dims(low_dose[sample,:,:,:],axis=0).astype(np.float32)
-                current_batch_high  = np.expand_dims(high_dose[sample,:,:,:],axis=0).astype(np.float32)
+    
+            print('\n=================')
+            # CV SET Average PSNR
+            total_PSNR = 0
+            for sample in range(len(low_dose_cv)):
+                current_batch_low  = np.expand_dims(low_dose_cv[sample,:,:,:],axis=0).astype(np.float32)
+                current_batch_high  = np.expand_dims(high_dose_cv[sample,:,:,:],axis=0).astype(np.float32)
                 
-                sess_result = sess.run([layer4,cost_PSNR],feed_dict={x:current_batch_low,y:current_batch_high})
+                sess_result = sess.run([layer5,cost_PSNR],feed_dict={x:current_batch_low,y:current_batch_high})
                 image=  sess_result[0]
-
                 real_PSNR = compare_psnr(np.squeeze(current_batch_high),np.squeeze(image))
-                print("==Printing : ", sample, " PSNR : ", sess_result[1]," Real : ",real_PSNR)
+                total_PSNR = total_PSNR + real_PSNR
+            print("CV Set: Current iter: ", iter, ' Average Peak Noise: ',total_PSNR/len(low_dose_cv))
+
+            # Test SET Average PSNR
+            total_PSNR = 0
+            for sample in range(len(low_dose_test)):
+                current_batch_low  = np.expand_dims(low_dose_test[sample,:,:,:],axis=0).astype(np.float32)
+                current_batch_high  = np.expand_dims(high_dose_test[sample,:,:,:],axis=0).astype(np.float32)
                 
-                plt.figure()
-                plt.imshow(np.squeeze(image),cmap='gray')
-                plt.savefig(save_location +str(iter) +"_"+str(sample) + "_denoised_"+ str(real_PSNR)+ ".png")
+                sess_result = sess.run([layer5,cost_PSNR],feed_dict={x:current_batch_low,y:current_batch_high})
+                image=  sess_result[0]
+                real_PSNR = compare_psnr(np.squeeze(current_batch_high),np.squeeze(image))
+                total_PSNR = total_PSNR + real_PSNR
+            print("Test Set: Current iter: ", iter, ' Average Peak Noise: ',total_PSNR/len(low_dose_test))
+            print('=================')
 
-                plt.figure()
-                plt.imshow(np.squeeze(current_batch_low),cmap='gray')
-                plt.savefig(save_location +str(iter) +"_"+str(sample) + "_noised_"+ str(real_PSNR)+ ".png")
+            # Print out the image
+            current_batch_low  = np.expand_dims(low_dose_test[-1,:,:,:],axis=0).astype(np.float32)
+            current_batch_high  = np.expand_dims(high_dose_test[-1,:,:,:],axis=0).astype(np.float32)  
 
-                plt.figure()
-                plt.imshow(np.squeeze(current_batch_high),cmap='gray')
-                plt.savefig(save_location + str(iter) +"_"+str(sample) + "_groundt_"+ str(real_PSNR)+ ".png")
-                plt.close('all')
-
-            current_batch_low  = np.expand_dims(low_dose[406,:,:,:],axis=0).astype(np.float32)
-            current_batch_high  = np.expand_dims(high_dose[406,:,:,:],axis=0).astype(np.float32)
-            sess_result = sess.run([layer4,cost_PSNR],feed_dict={x:current_batch_low,y:current_batch_high})
+            sess_result = sess.run([layer5,cost_PSNR],feed_dict={x:current_batch_low,y:current_batch_high})
             image=  sess_result[0]
-
             real_PSNR = compare_psnr(np.squeeze(current_batch_high),np.squeeze(image))
-            print("==Printing : ", 406, " PSNR : ", sess_result[1]," Real : ",real_PSNR)
-            
+            print("==Printing : ", sample, " PSNR : ", sess_result[1]," Real : ",real_PSNR)
+
             plt.figure()
             plt.imshow(np.squeeze(image),cmap='gray')
-            plt.savefig(save_location +str(iter) +"_"+str(406) + "_denoised_"+ str(real_PSNR)+ ".png")
+            plt.savefig(save_location +str(iter) +"_"+str(sample) + "_denoised_"+ str(real_PSNR)+ ".png")
 
             plt.figure()
             plt.imshow(np.squeeze(current_batch_low),cmap='gray')
@@ -232,12 +258,10 @@ with tf.Session() as sess:
 
             plt.figure()
             plt.imshow(np.squeeze(current_batch_high),cmap='gray')
-            plt.savefig(save_location + str(iter) +"_"+str(406) + "_groundt_"+ str(real_PSNR)+ ".png")
+            plt.savefig(save_location + str(iter) +"_"+str(sample) + "_groundt_"+ str(real_PSNR)+ ".png")
             plt.close('all')
-                
-            print("Current Ite : ", iter, " Current total cost: ", total_cost)
-            print('\n')
-        
+
+    
         cost_over_time.append(total_cost)
         total_cost=0
 
