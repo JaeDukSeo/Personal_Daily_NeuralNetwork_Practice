be4ebe2f94f3801fd7b041846daa7b0a262560c9
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-18 23:10:23 -0400

    's'

--- y_CVPR/a_FRRU/a.py
+++ y_CVPR/a_FRRU/a.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
+import matplotlib.pyplot as plt
 
+mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
 
+print(mnist.train.images.shape)
 
 
 # --- end code ---
