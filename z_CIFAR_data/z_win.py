3fd4e8f84507d138799fe7b24212139a9064aedf
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-20 17:04:54 -0400

    's'

--- z_CIFAR_data/z_win.py
+++ z_CIFAR_data/z_win.py
@@ -11,28 +11,32 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
+# make class
+class CNNLayer():
+    
+    def __init__(self,kernel,in_c,out_c,act,d_act):
+        self.w = tf.Variable(tf.random_normal([kernel,kernel,in_c,out_c]))
+        self.act,self.d_act = act,d_act
+        
+    def feedforward(self,input):
+        self.input  = input 
+        self.layer  = tf.nn.conv2d(input,self.w,strides=[1,1,1,1],padding='VALID')
+        self.layerA = self.act(self.layer)
+        return self.layerA
+
 # Get the Train data
 data = unpickle('cifar100python/train')
-train_image = data[b'data']
+train_batch = data[b'data']
 train_label = data[b'fine_labels']
 
 # Get the Test Data
 data = unpickle('cifar100python/test')
-test_image = data[b'data']
+test_batch = data[b'data']
 test_label = data[b'fine_labels']
 
-print(type(train_image))
-print(dir(train_image))
-print(train_image.shape)
-print(train_image.min())
-print(train_image.max())
-
-print(test_image.shape)
-print(test_image.min())
-print(test_image.max())
-
-
-
+# Normalize
+train_batch = (train_batch - train_batch.min(axis=0))/(train_batch.max(axis=0)-train_batch.min(axis=0))
+test_batch = (test_batch - test_batch.min(axis=0))/(test_batch.max(axis=0)-test_batch.min(axis=0))
 
 
 
