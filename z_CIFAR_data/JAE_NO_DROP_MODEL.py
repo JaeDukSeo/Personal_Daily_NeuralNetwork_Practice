2cb2b33294287dbc1b104a68bb1956c284c0ffee
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-23 12:49:27 -0400

    ''s

--- z_CIFAR_data/JAE_NO_DROP_MODEL.py
+++ z_CIFAR_data/JAE_NO_DROP_MODEL.py
@@ -96,28 +96,58 @@ class CNNLayer():
     tf.assign(self.w,self.w - self.m)
     return grad_pass,update_w
 
+class FNNLayer():
+    
+    def __init__(self,in_c,out_c,act,d_act):
+        with tf.device('/cpu:0'):
+            self.w = tf.Variable(tf.truncated_normal([in_c,out_c],stddev=0.05))
+            self.act,self.d_act = act,d_act
+            self.m,self.v = tf.Variable(tf.zeros_like(self.w)), tf.Variable(tf.zeros_like(self.w))
+
+    def feedforward(self,input):
+        self.input  = input 
+        self.layer  = tf.matmul(input,self.w)
+        self.layerA = self.act(self.layer)
+        return self.layerA
+
+    def backprop(self,gradient):
+        
+        grad_part_1 = gradient
+        grad_part_2 = self.d_act(self.layer)
+        grad_part_3 = self.input
+
+        grad = tf.matmul(tf.transpose(grad_part_3),tf.multiply(grad_part_1,grad_part_2))
+        pass_on_grad = tf.matmul(tf.multiply(grad_part_1,grad_part_2),tf.transpose(self.w))
+
+        grad_update = []
+        grad_update.append(tf.assign(self.m,tf.add(beta1*self.m, (1-beta1)*grad)))
+        grad_update.append(tf.assign(self.v,tf.add(beta2*self.v, (1-beta2)*grad**2)))
+
+        m_hat = self.m / (1-beta1)
+        v_hat = self.v / (1-beta2)
+        adam_middel = learning_rate/(tf.sqrt(v_hat) + adam_e)
+        grad_update.append(tf.assign(self.w,tf.subtract(self.w,tf.multiply(adam_middel,m_hat))))
+
+        return pass_on_grad,grad_update   
+
 
 
 # =========== Make Class  ===========
-l1 = CNNLayer(kernel=7,in_c=3,out_c=256)
-l2 = CNNLayer(kernel=1,in_c=256,out_c=256)
-
-l3 = CNNLayer(kernel=5,in_c=256,out_c=256)
-l4 = CNNLayer(kernel=1,in_c=256,out_c=256)
-
-l5 = CNNLayer(kernel=3,in_c=256,out_c=256)
-l6 = CNNLayer(kernel=1,in_c=256,out_c=256)
-
-l7 = CNNLayer(kernel=2,in_c=256,out_c=256)
-l8 = CNNLayer(kernel=1,in_c=256,out_c=256)
-
-l9 = CNNLayer(kernel=2,in_c=256,out_c=256)
-l10 = CNNLayer(kernel=1,in_c=256,out_c=10)
+l1 = CNNLayer(kernel=7,in_c=3,out_c=128)
+l2 = CNNLayer(kernel=1,in_c=128,out_c=128)
+l3 = CNNLayer(kernel=5,in_c=128,out_c=128)
+l4 = CNNLayer(kernel=1,in_c=128,out_c=128)
+l5 = CNNLayer(kernel=3,in_c=128,out_c=128)
+l6 = CNNLayer(kernel=1,in_c=128,out_c=128)
+l7 = CNNLayer(kernel=2,in_c=128,out_c=128)
+l8 = CNNLayer(kernel=1,in_c=128,out_c=128)
+l9 = CNNLayer(kernel=2,in_c=128,out_c=128)
+l10 = CNNLayer(kernel=1,in_c=128,out_c=10)
 
 # === Hyper Param ===
 num_epoch =  165000 
 num_epoch =  10001 
-learning_rate = 0.001
+learning_rate = 0.01
 print_size = 100
 
 batch_size = 100
@@ -151,40 +181,30 @@ layer9 = l9.feedforward(layer8)
 layer10 = l10.feedforward_avg(layer9)
 
 results = tf.reshape(layer10,(-1,NUM_CLASSES))
-tf_prediction = tf_softmax(results)
 
-# ====== my total cost =====
-# final_soft = tf_softmax(results)
-# cost = tf.reduce_mean(-1.0 * (y* tf.log(final_soft) + (1-y)*tf.log(1-final_soft)))
-# cost = tf.reduce_sum(-1.0 * (y* tf.log(final_soft) + (1-y)*tf.log(1-final_soft)))
-# tf.add_to_collection('losses', cost)
-# correct_prediction = tf.equal(tf.argmax(final_soft, 1), tf.argmax(y, 1))
-# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-# ====== my total cost =====
+final_soft = tf_softmax(results)
+cost = tf.reduce_sum(-1.0 * (y* tf.log(final_soft) + (1-y)*tf.log(1-final_soft)))
+correct_prediction = tf.equal(tf.argmax(final_soft, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# ===== Auto Train ====
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=results, labels=y, name='cross_entropy_per_example')
-cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
-tf.add_to_collection('losses', cross_entropy_mean)
-total_loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
-# optimizer = tf.train.MomentumOptimizer(learning_rate=tf_learning_rate, momentum=0.9).minimize(total_loss)
-# ===== Auto Train ====
+# === Auto train ===
+auto_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # === Back Propagation === 
-grad_10,grad_10w = l10.backprop_avg(tf.reshape(tf_prediction-y,[batch_size,1,1,NUM_CLASSES])+decay_propotoin_rate*(tf.reshape(tf_prediction-y,[batch_size,1,1,NUM_CLASSES])) )
-grad_9, grad_9w  = l9.backprop(grad_10+decay_propotoin_rate*(grad_10))
+grad_10,grad_10w = l10.backprop_avg(tf.reshape(final_soft-y,[batch_size,1,1,NUM_CLASSES]) )
+grad_9, grad_9w  = l9.backprop(grad_10)
 
 grad_8, grad_8w  = l8.backprop_avg(grad_9+decay_propotoin_rate*(grad_10+grad_9))
-grad_7, grad_7w  = l7.backprop(grad_8+decay_propotoin_rate*(grad_8))
+grad_7, grad_7w  = l7.backprop(grad_8)
 
 grad_6, grad_6w  = l6.backprop_avg(grad_7+decay_propotoin_rate*(grad_8+grad_7))
-grad_5, grad_5w  = l5.backprop(grad_6+decay_propotoin_rate*(grad_6))
+grad_5, grad_5w  = l5.backprop(grad_6)
 
 grad_4, grad_4w  = l4.backprop_avg(grad_5+decay_propotoin_rate*(grad_6+grad_5))
-grad_3, grad_3w  = l3.backprop(grad_4+decay_propotoin_rate*(grad_4))
+grad_3, grad_3w  = l3.backprop(grad_4)
 
 grad_2, grad_2w  = l2.backprop_avg(grad_3+decay_propotoin_rate*(grad_4+grad_3))
-grad_1, grad_1w  = l1.backprop(grad_2+decay_propotoin_rate*(grad_2))
+grad_1, grad_1w  = l1.backprop(grad_2)
 
 weight_update = grad_10w+grad_9w+ \
                 grad_8w+grad_7w+ \
