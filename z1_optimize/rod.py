6f38ec824a10ef40dbdb992790b51c9c025bfc9a
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-23 08:45:48 -0400

    's'

--- z1_optimize/rod.py
+++ z1_optimize/rod.py
@@ -4,6 +4,54 @@ import sys
 price = np.array([1,5,8,9,10,17,17,20])
 print(price.shape)
 
+
+
+
+
+
+INT_MIN = -9999999999999
+# Returns the best obtainable price for a rod of length n and
+# price[] as prices of different pieces
+def cutRod(price, n):
+    val = [0 for x in range(n+1)]
+    val[0] = 0
+    print(val)
+ 
+    # Build the table val[] in bottom up manner and return
+    # the last entry from the table
+    for i in range(1, n+1):
+        max_val = INT_MIN
+        for j in range(i):
+             max_val = max(max_val, price[j] + val[i-j-1])
+        val[i] = max_val
+        print(val)
+ 
+    return val[n]
+
+
+
+print(cutRod(price,0))
+print(cutRod(price,1))
+print(cutRod(price,2))
+print(cutRod(price,3))
+print(cutRod(price,4))
+print(cutRod(price,5))
+print(cutRod(price,6))
+print(cutRod(price,7))
+print(cutRod(price,8))
+
+
+
+sys.exit()
+
+
+
+
+
+
+
+
+
 global howmans
 howmans= 0 
 def bestprice(n):
