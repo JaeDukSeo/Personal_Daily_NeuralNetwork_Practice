91a71291022c0e39d778da820f4dd99345ea8a05
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-25 19:03:36 -0400

    's'

--- z_CIFAR_data/Simple_Net.py
+++ z_CIFAR_data/Simple_Net.py
@@ -6,9 +6,41 @@ import tensorflow as tf
 from six.moves import cPickle as pickle
 from read_10_data import get_data
 from sklearn.utils import shuffle
-
+import imgaug as ia
+from imgaug import augmenters as iaa
+ia.seed(1)
 np.random.seed(789)
 tf.set_random_seed(789)
+
+seq = iaa.Sequential([
+    iaa.Fliplr(0.5), # horizontal flips
+    iaa.Crop(percent=(0, 0.1)), # random crops
+    # Small gaussian blur with random sigma between 0 and 0.5.
+    # But we only blur about 50% of all images.
+    iaa.Sometimes(0.5,
+        iaa.GaussianBlur(sigma=(0, 0.5))
+    ),
+    # Strengthen or weaken the contrast in each image.
+    iaa.ContrastNormalization((0.75, 1.5)),
+    # Add gaussian noise.
+    # For 50% of all images, we sample the noise once per pixel.
+    # For the other 50% of all images, we sample the noise per pixel AND
+    # channel. This can change the color (not only brightness) of the
+    # pixels.
+    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+    # Make some images brighter and some darker.
+    # In 20% of all cases, we sample the multiplier once per channel,
+    # which can end up changing the color of the images.
+    iaa.Multiply((0.8, 1.2), per_channel=0.2),
+    # Apply affine transformations to each image.
+    # Scale/zoom them, translate/move them, rotate them and shear them.
+    iaa.Affine(
+        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+        rotate=(-25, 25),
+        shear=(-8, 8)
+    )
+], random_order=True) # apply augmenters in random order
 
 # activation functions here and there
 def tf_elu(x): return tf.nn.elu(x)
@@ -31,6 +63,21 @@ def d_tf_atan(x): return 1.0 / (1 + tf.square(x))
 
 # === Get Data ===
 train_images, train_labels, test_images,test_labels = get_data()
+
+print(train_images.shape)
+print(train_labels.shape)
+print(test_images.shape)
+print(test_labels.shape)
+
+train_images_aug = seq.augment_images(train_images)
+train_images = np.concatenate((train_images,train_images_aug),axis=0)
+train_labels = np.concatenate((train_labels,train_labels),axis=0)
+
+print(train_images.shape)
+print(train_labels.shape)
+print(test_images.shape)
+print(test_labels.shape)
+
 
 # === Hyper ===
 num_epoch =  10
