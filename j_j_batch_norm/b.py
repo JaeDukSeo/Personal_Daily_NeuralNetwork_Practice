46c7292f2937eca4829b5729e2cfbe325b8b7f1c
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-26 19:56:48 -0400

    's'

--- j_j_batch_norm/b.py
+++ j_j_batch_norm/b.py
@@ -6,25 +6,22 @@ tf.set_random_seed(678)
 # np.set_printoptions(precision=4)
 sess = tf.InteractiveSession()
 
-
-
 # create the test data and expand dim
-test_data = np.random.poisson(5,10000)
+test_data =50*np.random.rand() * np.random.normal(10, 10, 100) + 20
 
-print(test_data.shape)
-print(test_data.mean())
-print(test_data.max())
-print(test_data.min())
-plt.hist(test_data, bins=30)
+print(np.mean(test_data))
+print(np.var(test_data))
+print(np.max(test_data))
+print(np.min(test_data))
+plt.hist(test_data,bins='auto')
 plt.show()
 
-
 tf_batch_norm = tf.nn.batch_normalization(test_data,mean=np.mean(test_data),variance=np.var(test_data),scale=1.0,offset=0.0,variance_epsilon=1e8).eval()
-print(tf_batch_norm.shape)
-print(tf_batch_norm.mean())
-print(tf_batch_norm.max())
-print(tf_batch_norm.min())
-plt.hist(tf_batch_norm, bins=30)
+print(np.mean(tf_batch_norm))
+print(np.var(tf_batch_norm))
+print(np.max(tf_batch_norm))
+print(np.min(tf_batch_norm))
+plt.hist(tf_batch_norm, bins='auto')
 plt.show()
 
 
