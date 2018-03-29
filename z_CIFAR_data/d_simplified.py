a9ec997e206c6f5a2a65ae76e4ee2903dc88c4f0
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-29 17:53:05 -0400

    's'

--- z_CIFAR_data/d_simplified.py
+++ z_CIFAR_data/d_simplified.py
@@ -11,16 +11,7 @@ tf.set_random_seed(789)
 
 # activation functions here and there
 def tf_elu(x): return tf.nn.elu(x)
-def d_tf_elu(x): return tf_elu(x) + keep_prob 
-
-def tf_log(x): return tf.sigmoid(x) # dont use this 
-def d_tf_log(x): return tf_log(x) * (keep_prob - tf_log(x))
-
-def tf_tanh(x): return tf.nn.tanh(x) # dont use this 
-def d_tf_tanh(x): return keep_prob - tf.square(tf_tanh(x))
-
-def tf_atan(x): return tf.atan(x) # dont use this 
-def d_tf_atan(x): return keep_prob / (1 + tf.square(x))
+def d_tf_elu(x): return tf_elu(x) + 1.0 
 
 def tf_softmax(x): return tf.nn.softmax(x)
 
@@ -47,7 +38,7 @@ class Convolution_Layer():
         grad_part2 = self.d_act(self.layer)
         grad_part3 = self.input
 
-        grad_middle = tf.nn.multiply(grad_part1,grad_part2)
+        grad_middle = tf.multiply(grad_part1,grad_part2)
 
         grad_w = tf.nn.conv2d_backprop_filter(
             input = grad_part3,
@@ -57,8 +48,9 @@ class Convolution_Layer():
             padding='SAME'
         )
 
+        grad_input_shape = list(self.input.shape[1:])
         grad_pass = tf.nn.conv2d_backprop_input(
-            input_sizes = self.input.shape,
+            input_sizes = [batch_size] + grad_input_shape,
             filter = self.w,
             out_backprop = grad_middle,
             strides = [1,stride,stride,1],
@@ -66,19 +58,18 @@ class Convolution_Layer():
         )
 
         updatew = []
-        updatew.append(tf.assign(self.m,alpha*self.m + learning_rate*grad_w))
+        updatew.append(tf.assign(self.m,init_momentum_rate*self.m + init_learning_rate*grad_w))
+        updatew.append(tf.assign(self.w,self.w - self.m))
 
         return grad_pass,updatew
 
-    def backprop_adam():
-        return 4
 
 # === Get Data ===
 train_images, train_labels, test_images,test_labels = get_data()
 
 # === Hyper Parameter ===
 num_epoch =  200
-batch_size = 500
+batch_size = 100
 print_size = 1
 shuffle_size = 1
 divide_size = 4
@@ -86,12 +77,12 @@ divide_size = 4
 proportion_rate = 1000
 decay_rate = 0.08
 
-init_learning_rate = 0.01
-init_momentum_rate = 0.9
+init_learning_rate = 0.00001
+init_momentum_rate = 0.4
 init_dropout_rate  = np.random.uniform(0.8,0.9)
 init_noise_rate    = np.random.uniform(0.1,0.5)
 
-one_channel = 56
+one_channel = 128
 
 # === Make Class ===
 l1_1 = Convolution_Layer(3,3,one_channel,tf_elu,d_tf_elu)
@@ -156,29 +147,42 @@ layer5_2 = l5_2.feedforward(layer5_1,dropout=dropout_rate)
 layer5_s = l5_s.feedforward(layer4_add,2)
 layer5_add = layer5_s + layer5_2
 
-# --- final layer ----
-final_soft = tf.reshape(layer5_add,[batch_size,-1])
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits= final_soft,labels=y))
+# --- final layer ---- adding clip by value
+final_soft = tf.clip_by_value(tf_softmax(tf.reshape(layer5_add,[batch_size,-1])) ,1e-10,1.0)
+cost = tf.reduce_mean( -1.0 * ( y * tf.log(final_soft)) + (1.0-y) * tf.log( 1.0-final_soft)   )
 correct_prediction = tf.equal(tf.argmax(final_soft, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # --- auto train ---
-global_step = tf.Variable(0)
-auto_train = tf.train.MomentumOptimizer(learning_rate=learning_rate,momentum=momentum_rate).minimize(cost,global_step=global_step)
+# auto_train = tf.train.MomentumOptimizer(learning_rate=learning_rate,momentum=momentum_rate).minimize(cost)
 
+# --- manual back prop --- adding 1e-50 for numerical stability
+error_soft = tf.reshape( final_soft -y,[batch_size,1,1,10])
+grad5_s,grad5_sw = l5_s.backprop(error_soft,stride=2)
+grad5_2,grad5_2w = l5_2.backprop(error_soft)
+grad5_1,grad5_1w = l5_1.backprop(grad5_2,stride=2)
 
+grad4_s,grad4_sw = l4_s.backprop(grad5_1+grad5_s,stride=2)
+grad4_2,grad4_2w = l4_2.backprop(grad5_1+grad5_s)
+grad4_1,grad4_1w = l4_1.backprop(grad4_2,stride=2)
 
+grad3_s,grad3_sw = l3_s.backprop(grad4_1+grad4_s,stride=2)
+grad3_2,grad3_2w = l3_2.backprop(grad4_1+grad4_s)
+grad3_1,grad3_1w = l3_1.backprop(grad3_2,stride=2)
 
+grad2_s,grad2_sw = l2_s.backprop(grad3_1+grad3_s,stride=2)
+grad2_2,grad2_2w = l2_2.backprop(grad3_1+grad3_s)
+grad2_1,grad2_1w = l2_1.backprop(grad2_2,stride=2)
 
+grad1_s,grad1_sw = l1_s.backprop(grad2_1+grad2_s,stride=2)
+grad1_2,grad1_2w = l1_2.backprop(grad2_1+grad2_s)
+grad1_1,grad1_1w = l1_1.backprop(grad1_2,stride=2)
 
-
-
-
-
-
-
-
-
+grad_update = grad5_sw + grad5_2w + grad5_1w + \
+              grad4_sw + grad4_2w + grad4_1w + \
+              grad3_sw + grad3_2w + grad3_1w + \
+              grad2_sw + grad2_2w + grad2_1w + \
+              grad1_sw + grad1_2w + grad1_1w 
 
 
 
@@ -203,9 +207,9 @@ with tf.Session() as sess:
         for current_batch_index in range(0,int(len(train_images)/divide_size),batch_size):
             current_batch = train_images[current_batch_index:current_batch_index+batch_size,:,:,:]
             current_batch_label = train_labels[current_batch_index:current_batch_index+batch_size,:]
-            sess_results =  sess.run([cost,accuracy,auto_train],feed_dict={x: current_batch, y: current_batch_label, learning_rate:init_learning_rate,momentum_rate:init_momentum_rate,dropout_rate:init_dropout_rate})
+            sess_results =  sess.run([cost,accuracy,grad_update,final_soft,error_soft,correct_prediction],
+                                feed_dict={x: current_batch, y: current_batch_label, learning_rate:init_learning_rate,momentum_rate:init_momentum_rate,dropout_rate:init_dropout_rate})
 
-            sess_results[0] = sess_results[0] * 0.5
             print("current iter:", iter,' Drop Out Rate: %.3f'%init_dropout_rate,' learning rate: %.3f'%init_learning_rate ,
                 ' Current batach : ',current_batch_index," current cost: %.5f" % sess_results[0],' current acc: %.5f '%sess_results[1], end='\r')
             train_total_cost = train_total_cost + sess_results[0]
@@ -216,7 +220,7 @@ with tf.Session() as sess:
             current_batch = test_images[current_batch_index:current_batch_index+batch_size,:,:,:]
             current_batch_label = test_labels[current_batch_index:current_batch_index+batch_size,:]
             sess_results =  sess.run([cost,accuracy],feed_dict={x: current_batch, y: current_batch_label,dropout_rate:1.0})
-            sess_results[0] = sess_results[0] * 0.5
+
             print("Test Image Current iter:", iter,' Drop Out Rate: %.3f'%init_dropout_rate,' learning rate: %.3f'%init_learning_rate,
                  ' Current batach : ',current_batch_index, " current cost: %.5f" % sess_results[0],' current acc: %.5f '%sess_results[1], end='\r')
             test_total_cost = test_total_cost + sess_results[0]
