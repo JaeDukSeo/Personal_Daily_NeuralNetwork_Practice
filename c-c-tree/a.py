9e4a3575a5ad20bc0d28c14648d08eec9e2d9b11
JDSeo <jae.duk.seo@ryerson.ca>
2018-03-30 10:48:18 -0400

    's'

--- c-c-tree/a.py
+++ c-c-tree/a.py
@@ -1,15 +1,13 @@
 import os
 import numpy
 from matplotlib import pyplot as plt, cm
-import io
-import gzip
-import zipfile
+import io,gzip,zipfile
 import numpy as np,sys,os
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import plot, draw, show,ion
 from scipy.ndimage import imread
 from sklearn.utils import shuffle
 import tensorflow as tf
+from sklearn.preprocessing import OneHotEncoder
 
 tf.set_random_seed(789)
 np.random.seed(568)
@@ -30,9 +28,14 @@ def d_tf_log(x):return tf.sigmoid(x) * (1.0 - tf.sigmoid(x))
 def tf_elu(x): return tf.elu(x)
 def d_tf_elu(x): 
   mask1 = tf.cast(tf.greater(x,0),dtype=tf.flaot32)
-  maks2 = tf_elu(tf.less_equal(x,0))
+  maks2 = tf_elu(tf.cast(tf.less_equal(x,0),dtype=tf.float32) * x)
   return mask1 + mask2
 
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
 # ---- make class ----
 class ConLayer():
@@ -84,3 +87,40 @@ class ConLayer():
 
     return grad_pass,update_w
 
+
+# --- get data ---
+batch0 = unpickle(lstFilesDCM[0])
+batch1 = unpickle(lstFilesDCM[1])
+batch2 = unpickle(lstFilesDCM[2])
+batch3 = unpickle(lstFilesDCM[3])
+batch4 = unpickle(lstFilesDCM[4])
+onehot_encoder = OneHotEncoder(sparse=True)
+
+train_batch = np.vstack((batch0[b'data'],batch1[b'data'],batch2[b'data'],batch3[b'data'],batch4[b'data']))
+train_label = np.expand_dims(np.hstack((batch0[b'labels'],batch1[b'labels'],batch2[b'labels'],batch3[b'labels'],batch4[b'labels'])).T,axis=1).astype(np.float32)
+train_labels = onehot_encoder.fit_transform(train_label).toarray().astype(np.float32)
+
+test_batch = unpickle(lstFilesDCM[5])[b'data']
+test_label = np.expand_dims(np.array(unpickle(lstFilesDCM[5])[b'labels']),axis=0).T.astype(np.float32)
+test_labels = onehot_encoder.fit_transform(test_label).toarray().astype(np.float32)
+
+# Normalize data from 0 to 1
+train_batch = (train_batch - train_batch.min(axis=0))/(train_batch.max(axis=0)-train_batch.min(axis=0))
+test_batch = (test_batch - test_batch.min(axis=0))/(test_batch.max(axis=0)-test_batch.min(axis=0))
+
+# reshape data
+train_batch = np.reshape(train_batch,(len(train_batch),3,32,32))
+test_batch = np.reshape(test_batch,(len(test_batch),3,32,32))
+
+# rotate data
+train_images = np.rot90(np.rot90(train_batch,1,axes=(1,3)),3,axes=(1,2)).astype(np.float32)
+test_images = np.rot90(np.rot90(test_batch,1,axes=(1,3)),3,axes=(1,2)).astype(np.float32)
+
+
+
+
+
+
+
+
+# -- end code ---
