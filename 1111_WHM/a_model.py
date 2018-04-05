87ba771aeb737b48ecc82c1f37f825f0d6fe7b9c
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-05 19:10:55 -0400

    's'

--- 1111_WHM/a_model.py
+++ 1111_WHM/a_model.py
@@ -1,7 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-imgs_utrecht = np.load('utrecht_flair(128)aug.npy')
-mask_utrecht = np.load('utrecht_mask(128)aug.npy')
+
+imgs_utrecht = np.load('./data/utrecht_flair(128)aug.npy')
+mask_utrecht = np.load('./data/utrecht_mask(128)aug.npy')
 
 print(imgs_utrecht.shape)
 print(mask_utrecht.shape)
+
+temp  = imgs_utrecht[0,:,:,:]
+temp2 = mask_utrecht[0,:,:,:]
+for x in range(temp.shape[2]):
+    plt.imshow(temp[:,:,x],cmap='gray')
+    plt.pause(0.04)
+
+    plt.imshow(temp2[:,:,x],cmap='gray')
+    plt.pause(0.04)
+
+# -- end code --
