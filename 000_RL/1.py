1cb204df2f6cb5da22cef5b40d10252f7c9e15df
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-06 20:58:37 -0400

    's'

--- 000_RL/1.py
+++ 000_RL/1.py
@@ -14,4 +14,47 @@ for x in dir(env):
     except:
         pass
 
+#  there are 16 and 4
+print(env.observation_space.n,env.action_space.n)
+#Initialize table with all zeros
+Q = np.zeros([env.observation_space.n,env.action_space.n])
+
+# hyper
+lr = .8
+y = .95
+num_episodes = 1
+
+# Reward per each step
+rList = []
+print(rList)
+print(Q)
+print('-------')
+
+# this seems to be like some thing of num_epoch
+for i in range(num_episodes):
+
+    s = env.reset()
+    rAll = 0
+    d = False
+    j = 0
+
+    for j in range(19):
+        #Choose an action by greedily (with noise) picking from Q table
+        a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
+        #Get new state and reward from environment
+        s1,r,d,_ = env.step(a)
+        #Update Q-Table with new knowledge
+        Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
+        rAll += r
+        s = s1
+        if d == True:
+            break
+    rList.append(rAll)
+
+print('========================')
+print(len(rList))
+print(Q)
+print('-------')
+
+
 # -- end code --
