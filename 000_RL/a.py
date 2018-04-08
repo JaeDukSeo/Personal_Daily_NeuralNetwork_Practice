3ba67bda540971f5f4e83ee30670f58b6fc6f077
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-07 20:51:04 -0400

    's'

--- 000_RL/a.py
+++ 000_RL/a.py
@@ -1,17 +1,30 @@
 import gym,sys,numpy as np
-# import tensorflow as tf
+import tensorflow as tf
 
+from gym.envs.registration import register
+register(
+    id='FrozenLakeNotSlippery-v0',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name' : '4x4', 'is_slippery': False},
+    max_episode_steps=100,
+    reward_threshold=0.78, # optimum = .8196
+)
 
+# make the env
 env = gym.make('FrozenLake-v0')
+# print(env.observation_space)
+# print(env.action_space)
+# q_learning_table = np.zeros([env.observation_space.n,env.action_space.n])
+# print(q_learning_table)
+# print(env.render())
 
-print(env.observation_space)
-print(env.action_space)
-
-q_learning_table = np.zeros([env.observation_space.n,env.action_space.n])
-print(q_learning_table)
-
-print(env.render())
-
+# perfect step
+env = gym.make('FrozenLakeNotSlippery-v0')
+s = env.reset()
+perfect_step = [1,1,2,2,1,2]
+for x in perfect_step:
+    env.step(x)
+    env.render()
 
 
 
