81fd512df3f077bc80721f4fc39eab91fdb34ffe
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-07 20:11:07 -0400

    's'

--- 000_RL/1.py
+++ 000_RL/1.py
@@ -3,62 +3,39 @@ import numpy as np
 
 env = gym.make('FrozenLake-v0')
 
-
-
-#  there are 16 and 4
-print(env.observation_space)
-print(env.action_space)
-
-print(env.observation_space.n)
-print(env.action_space.n)
-
-sys.exit()
-
+np.random.seed(6789)
 #Initialize table with all zeros
 Q = np.zeros([env.observation_space.n,env.action_space.n])
-
-# hyper
-lr = .8
-y = .95
-num_episodes = 1
-
-# Reward per each step
+# Set learning parameters
+lr = .9
+y = .8
+num_episodes = 200
+#create lists to contain total rewards and steps per episode
+#jList = []
 rList = []
-print(rList)
-print(Q)
-print('-------')
-
-# this seems to be like some thing of num_epoch
 for i in range(num_episodes):
-
+    #Reset environment and get first new observation
     s = env.reset()
     rAll = 0
     d = False
     j = 0
-
-    for j in range(19):
-        
+    #The Q-Table learning algorithm
+    while j < 99:
+        j+=1
         #Choose an action by greedily (with noise) picking from Q table
         a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
-
-        print(a)
-
-        #Get new state and reward from environment (observation,reward,done)
+        #Get new state and reward from environment
         s1,r,d,_ = env.step(a)
-
         #Update Q-Table with new knowledge
-        Q[s,a] = Q[s,a] + lr * (r + y * np.max(Q[s1,:]) - Q[s,a] )
+        Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
         rAll += r
-        # print(s,'  ', d, ' ', s1)
         s = s1
         if d == True:
             break
+    #jList.append(j)
     rList.append(rAll)
 
-print('========================')
-print(len(rList))
-print(Q)
-print('-------')
+print(np.around(Q,3))
 
 
 # -- end code --
