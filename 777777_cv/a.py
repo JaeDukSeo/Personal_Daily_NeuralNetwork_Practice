87433aef36e9dee5ecfdcf9ec3d22edaf66137c8
JDSeo <jae.duk.seo@ryerson.ca>
2018-04-09 08:03:59 -0400

    's'

--- 777777_cv/a.py
+++ 777777_cv/a.py
@@ -1,5 +1,16 @@
 import numpy as np
+import os
 
 
+
+PathDicom = "./images/"
+lstFilesDCM = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom):
+    for filename in fileList:
+        if ".png" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM.append(os.path.join(dirName,filename))
+
+
+print(lstFilesDCM)
 
 # -- end code --
